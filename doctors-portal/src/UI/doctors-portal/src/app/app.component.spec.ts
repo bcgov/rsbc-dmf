46b460781f0dcc0daeb1996d922681c98d260f9c
Yossi Tamari <Yossi.Tamari@quartech.com>
2021-05-20 08:57:17 -0700

    fixed test

--- doctors-portal/src/UI/doctors-portal/src/app/app.component.spec.ts
+++ doctors-portal/src/UI/doctors-portal/src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('RSBC - DFT - Doctor\'s portal');
+    expect(compiled.querySelector('h1').textContent).toContain('RSBC - DFT - Doctor\'s portal');
   });
 });
