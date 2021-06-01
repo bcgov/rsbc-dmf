1cb3bb89427b02993937f5866529b20eadba03e2
Yossi Tamari <Yossi.Tamari@quartech.com>
2021-06-01 08:02:07 -0700

    refactoring

--- doctors-portal/src/UI/doctors-portal/src/app/app.component.ts
+++ doctors-portal/src/UI/doctors-portal/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { LoginService } from './login.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 
 export class AppComponent {
