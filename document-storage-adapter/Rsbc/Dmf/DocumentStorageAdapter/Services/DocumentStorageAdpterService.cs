54f0e7615741aed0d9051aa04f34aac3d3033813
George Walker <george@walkersoftware.ca>
2021-06-02 17:14:03 -0700

    Document Storage Adapter initial commit

---
+++ document-storage-adapter/Rsbc/Dmf/DocumentStorageAdapter/Services/DocumentStorageAdpterService.cs
@@ -0,0 +1,390 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Rsbc.Dmf.Interfaces;
+using Serilog;
+
+/** makes use of the AWS S3 SDK
+ 
+https://aws.amazon.com/sdk-for-net/
+
+*/
+
+namespace Rsbc.Dmf.DocumentStorageAdapter.Services
+{
+    // Default to require authorization
+    [Authorize]
+    public class DocumentStorageAdapterService : DocumentStorageAdapter.DocumentStorageAdapterBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<DocumentStorageAdapterService> _logger;
+
+        public DocumentStorageAdapterService(ILogger<DocumentStorageAdapterService> logger, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        public override Task<CreateFolderReply> CreateFolder(CreateFolderRequest request, ServerCallContext context)
+        {
+            var result = new CreateFolderReply();
+
+            var logFolder = WordSanitizer.Sanitize(request.FolderName);
+
+            var listTitle = GetDocumentListTitle(request.EntityName);
+
+            var _S3 = new S3(_configuration);
+
+            CreateDocumentLibraryIfMissing(listTitle, GetDocumentTemplateUrlPart(request.EntityName));
+
+            var folderExists = false;
+            try
+            {
+                var folder = _S3.GetFolder(listTitle, request.FolderName).GetAwaiter().GetResult();
+                if (folder != null) folderExists = true;
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, "Generic Exception creating folder");
+                folderExists = false;
+            }
+
+            if (folderExists)
+                result.ResultStatus = ResultStatus.Success;
+            else
+                try
+                {
+                    _S3.CreateFolder(GetDocumentListTitle(request.EntityName), request.FolderName)
+                        .GetAwaiter().GetResult();
+                    var folder = _S3.GetFolder(listTitle, request.FolderName).GetAwaiter()
+                        .GetResult();
+                    if (folder != null) result.ResultStatus = ResultStatus.Success;
+                }
+                catch (Exception e)
+                {
+                    result.ResultStatus = ResultStatus.Fail;
+                    result.ErrorDetail = $"ERROR in creating folder {logFolder}";
+                    Log.Error(e, result.ErrorDetail);
+                }
+
+            return Task.FromResult(result);
+        }
+
+        public override Task<FileExistsReply> FileExists(FileExistsRequest request, ServerCallContext context)
+        {
+            var result = new FileExistsReply();
+
+            var _S3 = new S3(_configuration);
+
+            List<S3.FileDetailsList> fileDetailsList = null;
+            try
+            {
+                fileDetailsList = _S3
+                    .GetFileDetailsListInFolder(GetDocumentTemplateUrlPart(request.EntityName), request.FolderName,
+                        request.DocumentType).GetAwaiter().GetResult();
+                if (fileDetailsList != null)
+
+                {
+                    var hasFile = fileDetailsList.Any(f => f.ServerRelativeUrl == request.ServerRelativeUrl);
+
+                    if (hasFile)
+                        result.ResultStatus = FileExistStatus.Exist;
+                    else
+                        result.ResultStatus = FileExistStatus.NotExist;
+                }
+            }
+
+            catch (Exception e)
+            {
+                result.ResultStatus = FileExistStatus.Error;
+                result.ErrorDetail = "Error determining if file exists";
+                Log.Error(e, result.ErrorDetail);
+            }
+
+            return Task.FromResult(result);
+        }
+
+        private string GetDocumentListTitle(string entityName)
+        {
+            string listTitle;
+            switch (entityName.ToLower())
+            {
+                case "account":
+                    listTitle = S3.DefaultDocumentListTitle;
+                    break;
+                case "application":
+                    listTitle = S3.ApplicationDocumentListTitle;
+                    break;
+                case "contact":
+                    listTitle = S3.ContactDocumentListTitle;
+                    break;
+                case "worker":
+                    listTitle = S3.WorkerDocumentListTitle;
+                    break;
+                case "event":
+                    listTitle = S3.EventDocumentListTitle;
+                    break;
+                case "federal_report":
+                    listTitle = S3.FederalReportListTitle;
+                    break;
+                case "licence":
+                    listTitle = S3.LicenceDocumentListTitle;
+                    break;
+                default:
+                    listTitle = entityName;
+                    break;
+            }
+
+            return listTitle;
+        }
+
+        private string GetDocumentTemplateUrlPart(string entityName)
+        {
+            var listTitle = "";
+            switch (entityName.ToLower())
+            {
+                case "account":
+                    listTitle = S3.DefaultDocumentUrlTitle;
+                    break;
+                case "application":
+                    listTitle = "adoxio_application";
+                    break;
+                case "contact":
+                    listTitle = S3.ContactDocumentListTitle;
+                    break;
+                case "worker":
+                    listTitle = "adoxio_worker";
+                    break;
+                case "event":
+                    listTitle = S3.EventDocumentListTitle;
+                    break;
+                case "federal_report":
+                    listTitle = S3.FederalReportListTitle;
+                    break;
+                case "licence":
+                    listTitle = S3.LicenceDocumentUrlTitle;
+                    break;
+            }
+
+            return listTitle;
+        }
+
+        private void CreateDocumentLibraryIfMissing(string listTitle, string documentTemplateUrl = null)
+        {
+            var _S3 = new S3(_configuration);
+            var exists = _S3.DocumentLibraryExists(listTitle).GetAwaiter().GetResult();
+            if (!exists)
+                _S3.CreateDocumentLibrary(listTitle, documentTemplateUrl).GetAwaiter().GetResult();
+        }
+
+        public override Task<DeleteFileReply> DeleteFile(DeleteFileRequest request, ServerCallContext context)
+        {
+            var result = new DeleteFileReply();
+
+            var logUrl = WordSanitizer.Sanitize(request.ServerRelativeUrl);
+
+            var _S3 = new S3(_configuration);
+
+            try
+            {
+                var success = _S3.DeleteFile(request.ServerRelativeUrl).GetAwaiter().GetResult();
+
+                if (success)
+                    result.ResultStatus = ResultStatus.Success;
+                else
+                    result.ResultStatus = ResultStatus.Fail;
+            }
+            catch (Exception e)
+            {
+                result.ResultStatus = ResultStatus.Fail;
+                result.ErrorDetail = $"ERROR in deleting file {logUrl}";
+                Log.Error(e, result.ErrorDetail);
+            }
+
+            return Task.FromResult(result);
+        }
+
+        public override Task<DownloadFileReply> DownloadFile(DownloadFileRequest request, ServerCallContext context)
+        {
+            var result = new DownloadFileReply();
+            var logUrl = WordSanitizer.Sanitize(request.ServerRelativeUrl);
+            var _S3 = new S3(_configuration);
+
+            try
+            {
+                var data = _S3.DownloadFile(request.ServerRelativeUrl).GetAwaiter().GetResult();
+
+                if (data != null)
+                {
+                    result.ResultStatus = ResultStatus.Success;
+                    result.Data = ByteString.CopyFrom(data);
+                }
+                else
+                {
+                    result.ResultStatus = ResultStatus.Fail;
+                }
+            }
+            catch (Exception e)
+            {
+                result.ResultStatus = ResultStatus.Fail;
+                result.ErrorDetail = $"ERROR in downloading file {logUrl}";
+                Log.Error(e, result.ErrorDetail);
+            }
+
+
+            return Task.FromResult(result);
+        }
+
+        public override Task<UploadFileReply> UploadFile(UploadFileRequest request, ServerCallContext context)
+        {
+            var result = new UploadFileReply();
+            var logFileName = WordSanitizer.Sanitize(request.FileName);
+            var logFolderName = WordSanitizer.Sanitize(request.FolderName);
+
+            var _S3 = new S3(_configuration);
+
+            CreateDocumentLibraryIfMissing(GetDocumentListTitle(request.EntityName),
+                GetDocumentTemplateUrlPart(request.EntityName));
+
+            try
+            {
+                var fileName = _S3.AddFile(GetDocumentTemplateUrlPart(request.EntityName),
+                    request.FolderName,
+                    request.FileName,
+                    request.Data.ToByteArray(), request.ContentType).GetAwaiter().GetResult();
+
+                result.FileName = fileName;
+                result.ResultStatus = ResultStatus.Success;
+            }
+            catch (Exception e)
+            {
+                result.ResultStatus = ResultStatus.Fail;
+                result.ErrorDetail = $"ERROR in uploading file {logFileName} to folder {logFolderName}";
+                Log.Error(e, result.ErrorDetail);
+            }
+
+            return Task.FromResult(result);
+        }
+
+        public override Task<FolderFilesReply> FolderFiles(FolderFilesRequest request, ServerCallContext context)
+        {
+            var result = new FolderFilesReply();
+
+            // Get the file details list in folder
+            List<S3.FileDetailsList> fileDetailsList = null;
+            var _S3 = new S3(_configuration);
+            try
+            {
+                fileDetailsList = _S3
+                    .GetFileDetailsListInFolder(GetDocumentTemplateUrlPart(request.EntityName), request.FolderName,
+                        request.DocumentType).GetAwaiter().GetResult();
+                if (fileDetailsList != null)
+
+                {
+                    // gRPC ensures that the collection has space to accept new data; no need to call a constructor
+                    foreach (var item in fileDetailsList)
+                    {
+                        // Sharepoint API responds with dates in UTC format
+                        var utcFormat = DateTimeStyles.AssumeUniversal | DateTimeStyles.AdjustToUniversal;
+                        DateTime parsedCreateDate, parsedLastModified;
+                        DateTime.TryParse(item.TimeCreated, CultureInfo.InvariantCulture, utcFormat,
+                            out parsedCreateDate);
+                        DateTime.TryParse(item.TimeLastModified, CultureInfo.InvariantCulture, utcFormat,
+                            out parsedLastModified);
+
+                        var newItem = new FileSystemItem
+                        {
+                            DocumentType = item.DocumentType,
+                            Name = item.Name,
+                            ServerRelativeUrl = item.ServerRelativeUrl,
+                            Size = int.Parse(item.Length),
+                            TimeCreated = Timestamp.FromDateTime(parsedCreateDate),
+                            TimeLastModified = Timestamp.FromDateTime(parsedLastModified)
+                        };
+
+                        result.Files.Add(newItem);
+                    }
+
+                    result.ResultStatus = ResultStatus.Success;
+                }
+            }
+            catch (Exception e)
+            {
+                result.ResultStatus = ResultStatus.Fail;
+                result.ErrorDetail = "Error getting SharePoint File List";
+                Log.Error(e, result.ErrorDetail);
+            }
+            
+
+            return Task.FromResult(result);
+        }
+
+        [AllowAnonymous]
+        public override Task<TokenReply> GetToken(TokenRequest request, ServerCallContext context)
+        {
+            var result = new TokenReply();
+            result.ResultStatus = ResultStatus.Fail;
+
+            var configuredSecret = _configuration["JWT_TOKEN_KEY"];
+            if (configuredSecret.Equals(request.Secret))
+            {
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuredSecret));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var jwtSecurityToken = new JwtSecurityToken(
+                    _configuration["JWT_VALID_ISSUER"],
+                    _configuration["JWT_VALID_AUDIENCE"],
+                    expires: DateTime.UtcNow.AddYears(5),
+                    signingCredentials: creds
+                );
+                result.Token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+                result.ResultStatus = ResultStatus.Success;
+            }
+            else
+            {
+                result.ErrorDetail = "Bad Request";
+            }
+
+            return Task.FromResult(result);
+        }
+
+        public override Task<TruncatedFilenameReply> GetTruncatedFilename(TruncatedFilenameRequest request,
+            ServerCallContext context)
+        {
+            var result = new TruncatedFilenameReply();
+            var logFileName = WordSanitizer.Sanitize(request.FileName);
+            var logFolderName = WordSanitizer.Sanitize(request.FolderName);
+            try
+            {
+                var _S3 = new S3(_configuration);
+
+                // Ask SharePoint whether this filename would be truncated upon upload
+                var listTitle = GetDocumentListTitle(request.EntityName);
+                var maybeTruncated =
+                    _S3.GetTruncatedFileName(request.FileName, listTitle, request.FolderName);
+                result.FileName = maybeTruncated;
+                result.ResultStatus = ResultStatus.Success;
+            }
+            catch (Exception e)
+            {
+                result.ResultStatus = ResultStatus.Fail;
+                result.ErrorDetail = $"ERROR in getting truncated filename {logFileName} for folder {logFolderName}";
+                Log.Error(e, result.ErrorDetail);
+            }
+            
+
+            return Task.FromResult(result);
+        }
+    }
+}
---
+++ document-storage-adapter/Rsbc/Dmf/DocumentStorageAdapter/Startup.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Rsbc.Dmf.DocumentStorageAdapter.Services;
+using Serilog;
+using Serilog.Debugging;
+using Serilog.Events;
+using Serilog.Exceptions;
+using Serilog.Sinks.Splunk;
+
+namespace Rsbc.Dmf.DocumentStorageAdapter
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddDefaultTokenProviders();
+
+            if (!string.IsNullOrEmpty(Configuration["JWT_TOKEN_KEY"]))
+                // Configure JWT authentication
+                services.AddAuthentication(o =>
+                {
+                    o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                }).AddJwtBearer(o =>
+                {
+                    o.SaveToken = true;
+                    o.RequireHttpsMetadata = false;
+                    o.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        RequireExpirationTime = false,
+                        ValidIssuer = Configuration["JWT_VALID_ISSUER"],
+                        ValidAudience = Configuration["JWT_VALID_AUDIENCE"],
+                        IssuerSigningKey =
+                            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT_TOKEN_KEY"]))
+                    };
+                });
+
+            services.AddAuthorization();
+
+            services.AddGrpc(options =>
+            {
+                options.EnableDetailedErrors = true;
+                options.MaxReceiveMessageSize = 256 * 1024 * 1024; // 256 MB
+                options.MaxSendMessageSize = 256 * 1024 * 1024; // 256 MB
+            });
+
+            // health checks. 
+            services.AddHealthChecks()
+                .AddCheck("file-manager-service", () => HealthCheckResult.Healthy("OK"));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseHealthChecks("/hc/ready", new HealthCheckOptions
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
+            app.UseHealthChecks("/hc/live", new HealthCheckOptions
+            {
+                // Exclude all checks and return a 200-Ok.
+                Predicate = _ => false
+            });
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<DocumentStorageAdapterService>();
+
+                endpoints.MapGet("/",
+                    async context =>
+                    {
+                        await context.Response.WriteAsync(
+                            "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                    });
+            });
+
+            // enable Splunk logger using Serilog
+            if (!string.IsNullOrEmpty(Configuration["SPLUNK_COLLECTOR_URL"]) &&
+                !string.IsNullOrEmpty(Configuration["SPLUNK_TOKEN"])
+            )
+            {
+                var fields = new CustomFields();
+                if (!string.IsNullOrEmpty(Configuration["SPLUNK_CHANNEL"]))
+                    fields.CustomFieldList.Add(new CustomField("channel", Configuration["SPLUNK_CHANNEL"]));
+                var splunkUri = new Uri(Configuration["SPLUNK_COLLECTOR_URL"]);
+                var upperSplunkHost = splunkUri.Host?.ToUpperInvariant() ?? string.Empty;
+
+                // Fix for bad SSL issues 
+
+
+                Log.Logger = new LoggerConfiguration()
+                    .Enrich.FromLogContext()
+                    .Enrich.WithExceptionDetails()
+                    .WriteTo.Console()
+                    .WriteTo.EventCollector(Configuration["SPLUNK_COLLECTOR_URL"],
+                        sourceType: "filemanagerservice", eventCollectorToken: Configuration["SPLUNK_TOKEN"],
+                        restrictedToMinimumLevel: LogEventLevel.Information,
+#pragma warning disable CA2000 // Dispose objects before losing scope
+                        messageHandler: new HttpClientHandler
+                        {
+                            ServerCertificateCustomValidationCallback = (message, cert, chain, errors) =>
+                            {
+                                return true;
+                            }
+                        }
+#pragma warning restore CA2000 // Dispose objects before losing scope
+                    )
+                    .CreateLogger();
+
+                SelfLog.Enable(Console.Error);
+
+                Log.Logger.Information("File Manager Service Container Started");
+            }
+            else
+            {
+                Log.Logger = new LoggerConfiguration()
+                    .Enrich.FromLogContext()
+                    .Enrich.WithExceptionDetails()
+                    .WriteTo.Console()
+                    .CreateLogger();
+            }
+        }
+    }
+}
