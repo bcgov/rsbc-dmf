fb3b81e58685f10f2d9e55f7c291173087c93811
Yossi Tamari <Yossi.Tamari@quartech.com>
2021-06-03 16:39:21 -0700

    dashboard and css fixes

--- doctors-portal/src/UI/doctors-portal/src/app/app-routing.module.ts
+++ doctors-portal/src/UI/doctors-portal/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { AuthGuard } from './auth.guard.service';
 import { DashboardComponent } from './dashboard/dashboard.component';
 // import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'search', component: SearchComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
