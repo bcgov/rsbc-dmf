a8d46baa4df65e808dfb060b79e95f32f7c3c280
George Walker <george@walkersoftware.ca>
2021-06-03 19:28:06 -0700

    S3DMFT-274 - Smart config

---
+++ phsa-adapter/src/ViewModels/SmartConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rsbc.Dmf.PhsaAdapter.ViewModels
+{
+    public class SmartConfiguration
+    {
+        public string Authorization_endpoint { get; set; }
+        public string Token_endpoint { get; set; }
+        public string Introspection_endpoint { get; set; }
+        public List<string> Capabilities { get; set; }
+    }
+}
