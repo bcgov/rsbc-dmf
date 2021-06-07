51bdf2a45ce38a0f6adf4cca29afe4daf2b5bb43
ElizabethWolfe <ejwolfe@gmail.com>
2021-06-07 07:07:02 -0700

    Update health check.

--- functional-tests/bdd-tests/DoctorsPortalHealthCheck.cs
+++ functional-tests/bdd-tests/DoctorsPortalHealthCheck.cs
@@ -1,4 +1,6 @@
-﻿using Xunit;
+﻿using OpenQA.Selenium;
+using Protractor;
+using Xunit;
 using Xunit.Gherkin.Quick;
 
 /*
@@ -9,7 +11,10 @@ Feature: DoctorsPortalHealthCheck
 @pipeline
 Scenario: Doctors' Portal Health Check
     When I click on the doctors' portal
-    Then the portal is displayed
+    And the portal is displayed
+    And I enter the login credentials
+    And I click on the Submit button
+    Then the DMER dashboard is displayed
 */
 
 namespace bdd_tests
@@ -26,10 +31,35 @@ namespace bdd_tests
         }
 
 
-        [Then(@"the portal is displayed")]
+        [And(@"the portal is displayed")]
         public void DoctorsPortalDisplayed()
         {
             ngDriver.WrappedDriver.PageSource.Contains("Welcome to the doctor's portal!");
+        }
+
+
+        [And(@"I enter the login credentials")]
+        public void LoginCredentials()
+        {
+        }
+
+
+        [And(@"I click on the Submit button")]
+        public void SubmitButton()
+        {
+            var submitButton = ngDriver.WrappedDriver.FindElement(By.CssSelector("button.mat-primary"));
+            submitButton.Click();
+        }
+
+
+        [Then(@"the DMER dashboard is displayed")]
+        public void DashboardDisplayed()
+        {
+            ngDriver.WrappedDriver.PageSource.Contains("Dashboard");
+
+            ngDriver.WrappedDriver.PageSource.Contains("Search DMER Case");
+
+            ngDriver.WrappedDriver.PageSource.Contains("Submitted DMER Forms");
         }
     }
 }
