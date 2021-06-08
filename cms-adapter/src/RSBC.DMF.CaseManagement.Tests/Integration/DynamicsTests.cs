f29e64c08b84d92fce222a648260b7c863dc61bb
Yossi Tamari <Yossi.Tamari@quartech.com>
2021-06-08 07:58:08 -0700

    cms adapter solution initial commit

---
+++ cms-adapter/src/RSBC.DMF.CaseManagement.Tests/Integration/DynamicsTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RSBC.DMF.CaseManagement.Dynamics;
+using RSBC.DMF.CaseManagement.Service;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace RSBC.DMF.CaseManagement.Tests.Integration
+{
+    public class DynamicsTests : WebAppTestBase
+    {
+        public DynamicsTests(ITestOutputHelper output, WebApplicationFactory<Startup> webApplicationFactory) : base(output, webApplicationFactory)
+        {
+        }
+
+        [Fact(Skip = RequiresDynamics)]
+        public async Task GetSecurityToken()
+        {
+            var tokenProvider = services.GetRequiredService<ISecurityTokenProvider>();
+            testLogger.LogDebug("Authorization: Bearer {0}", await tokenProvider.AcquireToken());
+        }
+    }
+}
