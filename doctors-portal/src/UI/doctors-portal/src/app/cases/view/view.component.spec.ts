c9562c5720f2a76603daf84c0e6f3a7bccdd811f
Yossi Tamari <Yossi.Tamari@quartech.com>
2021-06-17 16:59:24 -0700

    unit tests

--- doctors-portal/src/UI/doctors-portal/src/app/cases/view/view.component.spec.ts
+++ doctors-portal/src/UI/doctors-portal/src/app/cases/view/view.component.spec.ts
@@ -1,4 +1,8 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ConfigurationService } from 'src/app/shared/services/configuration.service';
+import { ConfigurationStubService } from 'src/app/shared/stubs/configuration.service.stub';
 
 import { ViewComponent } from './view.component';
 
@@ -8,9 +12,16 @@ describe('ViewComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ViewComponent ]
+      declarations: [ViewComponent],
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule
+      ],
+      providers: [
+        { provide: ConfigurationService, useClass: ConfigurationStubService }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
