0c4b85fa5e9d98f575d4efe5533a3874009e17cb
Yossi Tamari <Yossi.Tamari@quartech.com>
2021-07-02 10:46:58 -0700

    grpc service for case search

--- cms-adapter/src/RSBC.DMF.CaseManagement.Service/Services/CaseService.cs
+++ cms-adapter/src/RSBC.DMF.CaseManagement.Service/Services/CaseService.cs
@@ -1,6 +1,8 @@
+using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.Logging;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace RSBC.DMF.CaseManagement.Service
@@ -9,15 +11,27 @@ namespace RSBC.DMF.CaseManagement.Service
     public class CaseService : CaseManager.CaseManagerBase
     {
         private readonly ILogger<CaseService> _logger;
+        private readonly ICaseManager caseManager;
 
-        public CaseService(ILogger<CaseService> logger)
+        public CaseService(ILogger<CaseService> logger, ICaseManager caseManager)
         {
             _logger = logger;
+            this.caseManager = caseManager;
         }
 
-        public async override Task<DmerReply> SubmitDmer(DmerRequest request, ServerCallContext context)
+        public async override Task<SearchReply> Search(SearchRequest request, ServerCallContext context)
         {
-            return await Task.FromResult(new DmerReply { CaseId = request.CaseId });
+            var searchResult = await caseManager.CaseSearch(new CaseSearchRequest { ByCaseId = request.CaseId });
+
+            var reply = new SearchReply();
+            reply.Items.Add(searchResult.Items.Select(c => new Case
+            {
+                CaseId = c.Id,
+                CreatedBy = c.CreatedBy,
+                CreatedOn = Timestamp.FromDateTime(c.CreatedOn)
+            }));
+
+            return reply;
         }
     }
 }
