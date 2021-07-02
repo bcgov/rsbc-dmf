250f2626450321547eee86cbb5e93ab806e9131c
Yossi Tamari <Yossi.Tamari@quartech.com>
2021-07-02 09:09:40 -0700

    Dynamics connected service generated code and refactoring

---
+++ cms-adapter/src/RSBC.DMF.CaseManagement.Service/Services/CaseService.cs
@@ -0,0 +1,21 @@
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace RSBC.DMF.CaseManagement.Service
+{
+    public class CaseService : CaseManager.CaseManagerBase
+    {
+        private readonly ILogger<CaseService> _logger;
+
+        public CaseService(ILogger<CaseService> logger)
+        {
+            _logger = logger;
+        }
+
+        public async override Task<DmerReply> SubmitDmer(DmerRequest request, ServerCallContext context)
+        {
+            return await Task.FromResult(new DmerReply { CaseId = request.CaseId });
+        }
+    }
+}
--- cms-adapter/src/RSBC.DMF.CaseManagement.Service/Startup.cs
+++ cms-adapter/src/RSBC.DMF.CaseManagement.Service/Startup.cs
@@ -31,16 +31,16 @@ namespace RSBC.DMF.CaseManagement.Service
             {
                 app.UseDeveloperExceptionPage();
             }
-
             app.UseRouting();
-
+            app.UseAuthentication();
+            app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcService<CaseService>();
 
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client");
                 });
             });
         }
