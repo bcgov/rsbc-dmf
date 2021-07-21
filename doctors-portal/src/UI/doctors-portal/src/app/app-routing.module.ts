173045b9e874417b759768e3d21bd18c7f4a3b20
Yossi Tamari <Yossi.Tamari@quartech.com>
2021-07-21 11:48:55 -0700

    cleanup

--- doctors-portal/src/UI/doctors-portal/src/app/app-routing.module.ts
+++ doctors-portal/src/UI/doctors-portal/src/app/app-routing.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  // { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'cases', loadChildren: () => import('./cases/cases.module').then(m => m.CasesModule) },
 ];
