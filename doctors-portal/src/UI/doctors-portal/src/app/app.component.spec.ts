d96f53a23bc6115f2bdf8a8edc4b210b352735a4
Yossi Tamari <Yossi.Tamari@quartech.com>
2021-07-21 18:47:03 -0700

    fixed broken tests

--- doctors-portal/src/UI/doctors-portal/src/app/app.component.spec.ts
+++ doctors-portal/src/UI/doctors-portal/src/app/app.component.spec.ts
@@ -2,8 +2,6 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { LoginService } from './shared/services/login.service';
-import { LoginStubService } from './shared/stubs/login.service.stub';
 import { ConfigurationService } from './shared/services/configuration.service';
 import { ConfigurationStubService } from './shared/stubs/configuration.service.stub';
 
@@ -14,8 +12,7 @@ describe('AppComponent', () => {
         RouterTestingModule,
         HttpClientTestingModule
       ], providers: [
-        { provide: ConfigurationService, useClass: ConfigurationStubService },
-        { provide: LoginService, useClass: LoginStubService }
+        { provide: ConfigurationService, useClass: ConfigurationStubService }
       ],
       declarations: [
         AppComponent
--- doctors-portal/src/UI/doctors-portal/src/app/layout/header/header.component.spec.ts
+++ doctors-portal/src/UI/doctors-portal/src/app/layout/header/header.component.spec.ts
@@ -1,9 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { OAuthModule, OAuthService } from 'angular-oauth2-oidc';
 import { ConfigurationService } from 'src/app/shared/services/configuration.service';
-import { LoginService } from 'src/app/shared/services/login.service';
 import { ConfigurationStubService } from 'src/app/shared/stubs/configuration.service.stub';
-import { LoginStubService } from 'src/app/shared/stubs/login.service.stub';
 
 import { HeaderComponent } from './header.component';
 
@@ -15,8 +12,7 @@ describe('HeaderComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [HeaderComponent],
       providers: [
-        { provide: ConfigurationService, useClass: ConfigurationStubService },
-        { provide: LoginService, useClass: LoginStubService }
+        { provide: ConfigurationService, useClass: ConfigurationStubService }
       ]
     })
       .compileComponents();
