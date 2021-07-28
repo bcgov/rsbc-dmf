75546b4dadba59bf7868c918c922fa9861febd8f
ElizabethWolfe <ejwolfe@gmail.com>
2021-07-27 18:39:32 -0700

    Update Gherkin.

--- functional-tests/bdd-tests/DoctorsPortalHealthCheck.cs
+++ functional-tests/bdd-tests/DoctorsPortalHealthCheck.cs
@@ -8,24 +8,10 @@ using Xunit.Gherkin.Quick;
 /*
 Feature: DoctorsPortalHealthCheck
     As a medical professional
-    I want to test the doctors' portal
+    I want to perform basic tests on the doctors' portal
 
-@pipeline
 Scenario: Doctors' Portal Health Check
     When I log in to the doctors' portal
-    And the content is displayed for the doctors portal
-    And I enter the login credentials
-    And I click on the Submit button
-    And the content is displayed for the DMER dashboard
-    Then I log out of the portal
-
-@browseronly
-Scenario: Doctors' Portal Health Check with Cert
-    When I log in to the doctors' portal
-    And I accept the cert request
-    And the content is displayed for the doctors portal
-    And I enter the login credentials
-    And I click on the Submit button
     And the content is displayed for the DMER dashboard
     And I click on the DMER Forms tab
     And I click on the Case ID for 111
@@ -37,9 +23,6 @@ Scenario: Doctors' Portal Health Check with Cert
 
 Scenario: Vision Assessment
     When I log in to the doctors' portal
-    And I accept the cert request
-    And I enter the login credentials
-    And I click on the Submit button
     And I click on the DMER Forms tab
     And I click on the Case ID for 111
     And I refresh the page
