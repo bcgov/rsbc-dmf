f38324f506abbbe4aeacbe1e3f8d62a1fd513047
ElizabethWolfe <ejwolfe@gmail.com>
2021-08-11 10:53:33 -0700

    All areas of concern checkboxes.

--- functional-tests/bdd-tests/DMERSyncope.cs
+++ functional-tests/bdd-tests/DMERSyncope.cs
@@ -11,7 +11,7 @@ Feature: DMERSyncope.feature
     I want to confirm the syncope business rules for a DMER
 
 @pipeline
-Scenario: Syncope - Single Instance
+Scenario: Syncope Unexplained Single No Repeat
     When I log in to the doctors' portal
     And I click on the DMER Forms tab
     And I click on the Case ID for 111
@@ -22,7 +22,26 @@ Scenario: Syncope - Single Instance
     And I click on the Next button
     And I enter the Uncorrected Binocular Vision as 20
     And I click on the Next button
-    And I enter the syncope details
+    And I enter the unexplained single no repeat syncope details
+    And I click on the Next button
+    And I enter the medical opinion and confirmations
+    And I click on the Submit Form button
+    Then I log out of the portal
+
+@pipeline
+Scenario: Syncope All Areas of Concern
+    When I log in to the doctors' portal
+    And I click on the DMER Forms tab
+    And I click on the Case ID for 111
+    And I refresh the page
+    And I wait for the drivers licence field to have a value
+    And I click on the Next button
+    And the second page content is displayed
+    And I click on the Next button
+    And I enter the Uncorrected Binocular Vision as 20
+    And I click on the Next button
+    And I enter the unexplained single no repeat syncope details
+    And I enter the all areas of concern that apply to patient's syncope experience
     And I click on the Next button
     And I enter the medical opinion and confirmations
     And I click on the Submit Form button
--- functional-tests/bdd-tests/DMERSyncope.feature
+++ functional-tests/bdd-tests/DMERSyncope.feature
@@ -19,3 +19,22 @@ Scenario: Syncope Unexplained Single No Repeat
     And I enter the medical opinion and confirmations
     And I click on the Submit Form button
     Then I log out of the portal
+
+@pipeline
+Scenario: Syncope All Areas of Concern
+    When I log in to the doctors' portal
+    And I click on the DMER Forms tab
+    And I click on the Case ID for 111
+    And I refresh the page
+    And I wait for the drivers licence field to have a value
+    And I click on the Next button
+    And the second page content is displayed
+    And I click on the Next button
+    And I enter the Uncorrected Binocular Vision as 20
+    And I click on the Next button
+    And I enter the unexplained single no repeat syncope details
+    And I enter the all areas of concern that apply to patient's syncope experience
+    And I click on the Next button
+    And I enter the medical opinion and confirmations
+    And I click on the Submit Form button
+    Then I log out of the portal
