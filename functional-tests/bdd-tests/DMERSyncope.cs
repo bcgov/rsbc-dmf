49ef60812f9dd6b1595322ebb62f7f7f25575bf8
ElizabethWolfe <ejwolfe@gmail.com>
2021-08-11 10:33:41 -0700

    Update Syncope test.

--- functional-tests/bdd-tests/DMERSyncope.cs
+++ functional-tests/bdd-tests/DMERSyncope.cs
@@ -1,9 +1,17 @@
-Feature: DMERKnownMedicalConditions.feature
+﻿using OpenQA.Selenium;
+using Protractor;
+using System;
+using System.Threading;
+using Xunit;
+using Xunit.Gherkin.Quick;
+
+/*
+Feature: DMERSyncope.feature
     As a Driver Medical Fitness SME
-    I want to confirm the known medical conditions business rules for a DMER
+    I want to confirm the syncope business rules for a DMER
 
 @pipeline
-Scenario: Known Medical Conditions - Syncope
+Scenario: Syncope - Single Instance
     When I log in to the doctors' portal
     And I click on the DMER Forms tab
     And I click on the Case ID for 111
@@ -19,3 +27,12 @@ Scenario: Known Medical Conditions - Syncope
     And I enter the medical opinion and confirmations
     And I click on the Submit Form button
     Then I log out of the portal
+*/
+
+namespace bdd_tests
+{
+    [FeatureFile("./DMERSyncope.feature")]
+    public sealed class DMERSyncope : TestBase
+    {
+    }
+}
