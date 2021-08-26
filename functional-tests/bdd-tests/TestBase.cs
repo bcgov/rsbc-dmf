e4f0a9190e3b487ad2ebb8cb79a08db83bccd1df
ElizabethWolfe <ejwolfe@gmail.com>
2021-08-26 14:08:30 -0700

    Update automation.

--- functional-tests/bdd-tests/TestBase.cs
+++ functional-tests/bdd-tests/TestBase.cs
@@ -686,10 +686,6 @@ namespace bdd_tests
                 // select 'Typical Vasovagal' for 'Syncopal Expression is:'
                 var typicalVasovagal = ngDriver.WrappedDriver.FindElement(By.Id("ejhgp9v-typicalVasovagal"));
                 typicalVasovagal.Click();
-
-                // select 'Yes' for 'Has the patient experienced a Syncopal Event in the past 1 Year?'
-                var pastSyncopalEventPastYearYes = ngDriver.WrappedDriver.FindElement(By.Id("e9pkjt-y"));
-                pastSyncopalEventPastYearYes.Click();
             }
 
             // DMERSyncope.feature :: Syncope Vasovagal, Recurrent, Atypical
@@ -709,8 +705,8 @@ namespace bdd_tests
                 var atypicalVasovagal = ngDriver.WrappedDriver.FindElement(By.Id("ejhgp9v-atypicalVasovagal"));
                 atypicalVasovagal.Click();
 
-                // select 'Yes' for 'Has the patient experienced a Syncopal Event in the past 7 days?'
-                var SyncopalEventPastSevenDaysYes = ngDriver.WrappedDriver.FindElement(By.Id("ecvhp5f-y"));
+                // select 'No' for 'Has the patient experienced a Syncopal Event in the past 7 days?'
+                var SyncopalEventPastSevenDaysYes = ngDriver.WrappedDriver.FindElement(By.Id("e9pkjt-n"));
                 SyncopalEventPastSevenDaysYes.Click();
             }
 
