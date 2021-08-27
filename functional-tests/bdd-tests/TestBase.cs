df0a41fa6d1e2f1c9c8e476dd3606c4d81414dee
ElizabethWolfe <ejwolfe@gmail.com>
2021-08-27 10:21:31 -0700

    Cardio automation updates.

--- functional-tests/bdd-tests/TestBase.cs
+++ functional-tests/bdd-tests/TestBase.cs
@@ -824,31 +824,68 @@ namespace bdd_tests
                 var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Id("ejbv4y-no"));
                 ICDTherapyDeliveredNo.Click();
 
+                /*
+                 * Coronary Artery Disease
+                 */
+
+                /*
+                 * Treatments
+                 */
+
                 // select 'Coronary Artery Disease'
+                //name
+                //data[yornCORO_1_a]
+                var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Name(""));
+                ICDTherapyDeliveredNo.Click();
 
                 // select 'No' for 'CAD has been treated with CABG?'
+                //epveh8e-n
+                var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Id(""));
+                ICDTherapyDeliveredNo.Click();
 
                 // select 'Yes' for 'CAD has been treated with CABG?'
+                //epveh8e-y
+                var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Id(""));
+                ICDTherapyDeliveredNo.Click();
 
                 /**************************** 
                     Arrhythmias and Pacemaker
                 *****************************/
 
                 // select 'Arrhythmias and Pacemaker'
+                //name
+                //data[yornCORO_1_d]
+                var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Name(""));
+                ICDTherapyDeliveredNo.Click();
 
                 // select 'Pacemaker Not Applicable' for 'Has the patient received a Pacemaker?'
+                //eoi3ub-pacemakerNotApplicable
+                var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Id(""));
+                ICDTherapyDeliveredNo.Click();
 
                 // select 'Recommended, patient declined' for 'Has the patient received a Pacemaker?'
+                var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Id(""));
+                ICDTherapyDeliveredNo.Click();
 
                 // select 'No, scheduled to occur' for 'Has the patient received a Pacemaker?'
+                var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Id(""));
+                ICDTherapyDeliveredNo.Click();
 
                 // select 'Yes' for 'Has the patient received a Pacemaker?'
+                var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Id(""));
+                ICDTherapyDeliveredNo.Click();
 
                 // select 'Scheduled to occur' for 'Cardiac Ablation'
+                var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Id(""));
+                ICDTherapyDeliveredNo.Click();
 
                 // select 'Within the past 3 Months' for 'Cardiac Ablation'
+                var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Id(""));
+                ICDTherapyDeliveredNo.Click();
 
                 // select 'No' for 'Cardiac Ablation'
+                var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Id(""));
+                ICDTherapyDeliveredNo.Click();
 
                 /***************
                     Sustained VT
@@ -859,9 +896,11 @@ namespace bdd_tests
                 // select 'VF with no reversible cause'
 
                 // select 'No' for 'Experienced within the past 6 months?'
-
+                var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Id(""));
+                ICDTherapyDeliveredNo.Click();
                 // select 'Yes' for 'Experienced within the past 6 months?'
-
+                var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Id(""));
+                ICDTherapyDeliveredNo.Click();
                 /******************************
                     Hemodynamically Unstable VT
                 ******************************/
@@ -869,9 +908,11 @@ namespace bdd_tests
                 // select 'Hemodynamically Unstable VT'
 
                 // select 'No' for 'Experienced within the past 6 months?'
-
+                var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Id(""));
+                ICDTherapyDeliveredNo.Click();
                 // select 'Yes' for 'Experienced within the past 6 months?'
-
+                var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Id(""));
+                ICDTherapyDeliveredNo.Click();
                 /************ 
                     AV Blocks
                 ************/
@@ -893,6 +934,8 @@ namespace bdd_tests
                 // select 'PAC or PVC'
                 //name
                 //data[checkCORO_HF_a15]
+                var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Name(""));
+                ICDTherapyDeliveredNo.Click();
 
                 // select 'No' for 'With impaired Level of Consciousness caused by cerebral ischemia?'
 
@@ -905,6 +948,8 @@ namespace bdd_tests
                 // select 'Sinus Node Dysfunction'
                 //name
                 //data[checkCORO_HF_a17]
+                var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Name(""));
+                ICDTherapyDeliveredNo.Click();
 
                 /***************************** 
                     Paroxysmal SVT, AF, or AFL
@@ -913,25 +958,32 @@ namespace bdd_tests
                 // select 'Paroxysmal SVT, AF, or AFL'
                 //name
                 //data[checkCORO_HF_a10]
-
+                var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Name(""));
+                ICDTherapyDeliveredNo.Click();
                 // select 'No' for 'Treated with therapy, no recurrence within past 3 months'
                 //enn2tu-n
-
+                var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Id(""));
+                ICDTherapyDeliveredNo.Click();
                 // select 'Yes' for 'Treated with therapy, no recurrence within past 3 months'
                 //enn2tu-y
-
+                var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Id(""));
+                ICDTherapyDeliveredNo.Click();
                 // select 'No additional detail available' for 'Condition has been treated with:'
                 //e52bjgd-noAdditionalDetailAvailable
-
+                var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Id(""));
+                ICDTherapyDeliveredNo.Click();
                 // select 'AFL, they have had a successful isthmus ablation with proven establishment of bidirectional isthmus block' for 'Condition has been treated with:'
                 //e52bjgd-aflTheyHaveHadASuccessfulIsthmusAblationWithProvenEstablishmentOfBidirectionalIsthmusBlock
-
+                var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Id(""));
+                ICDTherapyDeliveredNo.Click();
                 // select 'AF, they have had AV node ablation and pacemaker implantation and meet the standard for pacemaker treatment' for 'Condition has been treated with:'
                 //e52bjgd-afTheyHaveHadAvNodeAblationAndPacemakerImplantationAndMeetTheStandardForPacemakerTreatment
-
+                var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Id(""));
+                ICDTherapyDeliveredNo.Click();
                 // select 'SVT, it has been successfully treated with radiofrequency ablation' for 'Condition has been treated with:'
                 //e52bjgd-svtItHasBeenSuccessfullyTreatedWithRadiofrequencyAblation
-
+                var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Id(""));
+                ICDTherapyDeliveredNo.Click();
 
                 // select 'Heart Failure'
 
@@ -1018,15 +1070,23 @@ namespace bdd_tests
 
                 // select 'No' for 'Is the patient considered stable on immunotherapy?'
                 //e0r0v2s-n
+                var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Id(""));
+                ICDTherapyDeliveredNo.Click();
 
                 // select 'Yes' for 'Is the patient considered stable on immunotherapy?'
                 //e0r0v2s-y
+                var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Id(""));
+                ICDTherapyDeliveredNo.Click();
 
                 // select 'No' for 'Transplanted within the past 6 months?'
                 //ecev5di-n
+                var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Id(""));
+                ICDTherapyDeliveredNo.Click();
 
                 // select 'Yes' for 'Transplanted within the past 6 months?'
                 //ecev5di-y
+                var ICDTherapyDeliveredNo = ngDriver.WrappedDriver.FindElement(By.Id(""));
+                ICDTherapyDeliveredNo.Click();
             }
         }
 
