3974b9c7bd891930b9bae18e57e186a5c7944c77
Yossi Tamari <Yossi.Tamari@quartech.com>
2021-08-27 10:13:33 -0700

    dmer case search

--- cms-adapter/src/Rsbc.Dmf.CaseManagement.Tests/Integration/DynamicsTests.cs
+++ cms-adapter/src/Rsbc.Dmf.CaseManagement.Tests/Integration/DynamicsTests.cs
@@ -1,10 +1,8 @@
-﻿using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc.Testing;
+﻿using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Rsbc.Dmf.CaseManagement.Dynamics;
 using Rsbc.Dmf.CaseManagement.Service;
-using Shouldly;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
@@ -22,27 +20,6 @@ namespace Rsbc.Dmf.CaseManagement.Tests.Integration
         {
             var tokenProvider = services.GetRequiredService<ISecurityTokenProvider>();
             testLogger.LogDebug("Authorization: Bearer {0}", await tokenProvider.AcquireToken());
-        }
-
-        [Fact(Skip = RequiresDynamics)]
-        public async Task CanConnectToDynamics()
-        {
-            var caseManager = services.GetRequiredService<ICaseManager>();
-            var result = await caseManager.CaseSearch(new CaseSearchRequest());
-            result.ShouldNotBeNull().Items.ShouldBeEmpty();
-        }
-
-        [Fact(Skip = RequiresDynamics)]
-        public async Task CanSetFlags()
-        {
-            List<Flag> flags = new List<Flag>()
-            {
-                new Flag(){Description  = "testFlag - 1", Id = "flagTestItem1"},
-                new Flag(){Description  = "testFlag - 2", Id = "flagTestItem2"},
-            };
-            var caseManager = services.GetRequiredService<ICaseManager>();
-            var result = await caseManager.SetCaseFlags("222", false, flags, testLogger);
-            result.ShouldNotBeNull().Success.ShouldBe(true);
         }
     }
 }
