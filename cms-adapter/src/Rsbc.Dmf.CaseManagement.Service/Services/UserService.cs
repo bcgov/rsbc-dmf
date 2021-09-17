518fe67f8cdee1d3adb69a2c84038039d6553ef4
Yossi Tamari <Yossi.Tamari@quartech.com>
2021-09-17 15:43:01 -0700

    login WIP

---
+++ cms-adapter/src/Rsbc.Dmf.CaseManagement.Service/Services/UserService.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Core;
+using System.Threading.Tasks;
+
+namespace Rsbc.Dmf.CaseManagement.Service.Services
+{
+    public class UserService : UserManager.UserManagerBase
+    {
+        private readonly IUserManager userManager;
+
+        public UserService(IUserManager userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public async override Task<UsersSearchReply> Search(UsersSearchRequest request, ServerCallContext context)
+        {
+            return await base.Search(request, context);
+        }
+
+        public async override Task<UserLoginReply> Login(UserLoginRequest request, ServerCallContext context)
+        {
+            return await base.Login(request, context);
+        }
+    }
+}
