d89b3bfd28df837259af0ca7bbd0bb86a8c54ec3
Yossi Tamari <Yossi.Tamari@quartech.com>
2021-09-17 15:43:01 -0700

    login WIP

---
+++ cms-adapter/src/Rsbc.Dmf.CaseManagement/UserManager.cs
@@ -0,0 +1,63 @@
+﻿using Rsbc.Dmf.CaseManagement.Dynamics;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Rsbc.Dmf.CaseManagement
+{
+    public interface IUserManager
+    {
+        Task<GetUsersResponse> GetUsers(GetUsersRequest request);
+
+        Task<LoginUserResponse> LoginUser(LoginUserRequest request);
+    }
+
+    public class GetUsersRequest
+    {
+        public string ByBcscGuid { get; set; }
+    }
+
+    public class GetUsersResponse
+    {
+        public IEnumerable<User> Items { get; set; }
+    }
+
+    public class LoginUserRequest
+    {
+        public string ExternalSystem { get; set; }
+        public string ExternalSystemUserId { get; set; }
+    }
+
+    public class LoginUserResponse
+    {
+        public string Userid { get; set; }
+    }
+
+    public class User
+    {
+        public string Id { get; set; }
+        public string ExternalSystem { get; set; }
+        public string ExternalSystemUserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    internal class UserManager : IUserManager
+    {
+        private readonly DynamicsContext dynamicsContext;
+
+        public UserManager(DynamicsContext dynamicsContext)
+        {
+            this.dynamicsContext = dynamicsContext;
+        }
+
+        public Task<GetUsersResponse> GetUsers(GetUsersRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<LoginUserResponse> LoginUser(LoginUserRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
