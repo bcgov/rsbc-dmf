dc0c1f09f109592f6b6f8c82b5ef2ba21e89ca6b
George Walker <george@walkersoftware.ca>
2021-11-17 00:49:06 -0800

    implement github action format productversion

--- doctors-portal/src/API/RSBC.DMF.DoctorsPortal.API/Controllers/ApplicationVersionInfoController.cs
+++ doctors-portal/src/API/RSBC.DMF.DoctorsPortal.API/Controllers/ApplicationVersionInfoController.cs
@@ -31,22 +31,33 @@ namespace RSBC.DMF.DoctorsPortal.API.Controllers
             Assembly assembly = GetType().GetTypeInfo().Assembly;
             DateTime creationTime = System.IO.File.GetLastWriteTimeUtc(assembly.Location);
             FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
-            string fileVersion = fvi.ProductVersion;
-            
-            /*
-            SourceCommit = _configuration["OPENSHIFT_BUILD_COMMIT"],
-            SourceRepository = _configuration["OPENSHIFT_BUILD_SOURCE"],
-            SourceReference = _configuration["OPENSHIFT_BUILD_REFERENCE"],
-            */
+            string productVersion = fvi.ProductVersion;
+            string fileVersion = fvi.FileVersion;
+                       
+            // The OPENSHIFT_ environment variables are only set during an OpenShift build.
+
+            string sourceCommit = _configuration["OPENSHIFT_BUILD_COMMIT"];
+            string sourceRepository = _configuration["OPENSHIFT_BUILD_SOURCE"];
+            string sourceReference = _configuration["OPENSHIFT_BUILD_REFERENCE"];
+
+            // Allow version info to be obtained from a Github action build
+            var strings = productVersion.Split("!");
+            if (strings.Length == 5 )
+            {
+                sourceRepository = strings[0] + "/" + strings[1];
+                sourceReference = strings[2];
+                sourceCommit = strings[3];
+                fileVersion += "." + strings[4];
+            }
 
             ApplicationVersionInfo avi = new ApplicationVersionInfo
             {
                 BaseUri = _configuration["BASE_URI"],
                 BasePath = _configuration["BASE_PATH"],
                 Environment = _configuration["ASPNETCORE_ENVIRONMENT"],
-                SourceCommit = _configuration["OPENSHIFT_BUILD_COMMIT"],
-                SourceRepository = _configuration["OPENSHIFT_BUILD_SOURCE"],
-                SourceReference = _configuration["OPENSHIFT_BUILD_REFERENCE"],
+                SourceCommit = sourceCommit,
+                SourceRepository = sourceRepository,
+                SourceReference = sourceReference,
                 FileCreationTime = creationTime.ToString("O"), // Use the round trip format as it includes the time zone.
                 FileVersion = fileVersion
             };
