c621b212e9af6f30843761c3eb9af018cf7e0f3c
George Walker <george@walkersoftware.ca>
2021-12-14 17:45:27 -0800

    angular code coverage

--- doctors-portal/src/UI/doctors-portal/karma.conf.js
+++ doctors-portal/src/UI/doctors-portal/karma.conf.js
@@ -1,57 +1,50 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-const path = require('path');
+//const path = require('path');
 
 module.exports = function (config) {
   config.set({
-    files: [
-      'src/**/*.js',
-      'test/**/*.js'
-    ],
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: ['karma-coverage',
+
+    frameworks: ["jasmine", "karma-typescript", '@angular-devkit/build-angular'],    
+
+    plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),      
+      require('karma-coverage'),
+      require('karma-typescript'),
+      require('karma-typescript-angular2-transform'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
-    },
-    // optionally, configure the reporter
-    coverageReporter: {
-      type : 'json',
-      dir : 'coverage/'
-    },
+
     preprocessors: {
-      // source files, that you wanna generate coverage for
-      // do not include tests or libraries
-      // (these files will be instrumented by Istanbul)
-      'src/**/*.js': ['coverage']
+        "**/*.ts": ["coverage", "karma-typescript"],        
     },
-    reporters: ['coverage','progress', 'kjhtml'],
+
+    karmaTypescriptConfig: {
+        bundlerOptions: {
+            entrypoints: /\.spec\.ts$/,
+            transforms: [
+                require("karma-typescript-angular2-transform")
+            ]
+        }
+    },
+
+    // configure the coverage reporter
+    coverageReporter: {
+    type : 'json',
+    includeAllSources: false
+    },
+
+    reporters: ['progress', 'karma-typescript','coverage'],    
+
+    // optionally, configure the reporter
+    
+    browsers: ['ChromeHeadless'],
+
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['ChromeHeadlessNoSandbox'],
-        customLaunchers: {
-          ChromeHeadlessNoSandbox: {
-            base: 'ChromeHeadless',
-            flags: ['--no-sandbox']
-          }
-        },
-    singleRun: true,
-    restartOnFileChange: true
+    autoWatch: false,
+    singleRun: true
   });
 };
