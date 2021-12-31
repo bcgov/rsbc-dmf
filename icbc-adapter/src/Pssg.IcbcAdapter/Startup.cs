8fc5946df7e3a91512e396afe0c2b65faf0661ab
George Walker <george@walkersoftware.ca>
2021-12-31 15:33:18 -0800

    S3DMFT-1952 - Initial version of a C# ICBC Adapter

---
+++ icbc-adapter/src/Pssg.IcbcAdapter/Startup.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Pssg.IcbcAdapter.Services;
+using Serilog;
+using Serilog.Debugging;
+using Serilog.Events;
+using Serilog.Exceptions;
+using Serilog.Sinks.Splunk;
+
+namespace Pssg.IcbcAdapter
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders =
+                    ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+            });
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddDefaultTokenProviders();
+
+            if (!string.IsNullOrEmpty(Configuration["JWT_TOKEN_KEY"]))
+                // Configure JWT authentication
+                services.AddAuthentication(o =>
+                {
+                    o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                }).AddJwtBearer(o =>
+                {
+                    o.SaveToken = true;
+                    o.RequireHttpsMetadata = false;
+                    o.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        RequireExpirationTime = false,
+                        ValidIssuer = Configuration["JWT_VALID_ISSUER"],
+                        ValidAudience = Configuration["JWT_VALID_AUDIENCE"],
+                        IssuerSigningKey =
+                            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT_TOKEN_KEY"]))
+                    };
+                });
+
+            services.AddAuthorization();
+
+            // basic REST controller for Dynamics.
+
+            services.AddControllers(options => options.EnableEndpointRouting = false);
+
+            services.AddGrpc(options =>
+            {
+                options.EnableDetailedErrors = true;
+                options.MaxReceiveMessageSize = 256 * 1024 * 1024; // 256 MB
+                options.MaxSendMessageSize = 256 * 1024 * 1024; // 256 MB
+            });
+
+            // health checks. 
+            services.AddHealthChecks()
+                .AddCheck("document-storage-adapter", () => HealthCheckResult.Healthy("OK"));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.UseForwardedHeaders();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseHealthChecks("/hc/ready", new HealthCheckOptions
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
+            app.UseHealthChecks("/hc/live", new HealthCheckOptions
+            {
+                // Exclude all checks and return a 200-Ok.
+                Predicate = _ => false
+            });
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<IcbcAdapterService>();
+
+                endpoints.MapGet("/",
+                    async context =>
+                    {
+                        await context.Response.WriteAsync(
+                            "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                    });
+
+                endpoints.MapControllers();
+
+                
+            });
+
+            // enable Splunk logger using Serilog
+            if (!string.IsNullOrEmpty(Configuration["SPLUNK_COLLECTOR_URL"]) &&
+                !string.IsNullOrEmpty(Configuration["SPLUNK_TOKEN"])
+            )
+            {
+                var fields = new CustomFields();
+                if (!string.IsNullOrEmpty(Configuration["SPLUNK_CHANNEL"]))
+                    fields.CustomFieldList.Add(new CustomField("channel", Configuration["SPLUNK_CHANNEL"]));
+                var splunkUri = new Uri(Configuration["SPLUNK_COLLECTOR_URL"]);
+                
+                // Fix for bad SSL issues 
+
+
+                Log.Logger = new LoggerConfiguration()
+                    .Enrich.FromLogContext()
+                    .Enrich.WithExceptionDetails()
+                    .WriteTo.Console()
+                    .WriteTo.EventCollector(Configuration["SPLUNK_COLLECTOR_URL"],
+                        sourceType: "documentstorage", eventCollectorToken: Configuration["SPLUNK_TOKEN"],
+                        restrictedToMinimumLevel: LogEventLevel.Information,
+#pragma warning disable CA2000 // Dispose objects before losing scope
+                        messageHandler: new HttpClientHandler
+                        {
+                            ServerCertificateCustomValidationCallback = (message, cert, chain, errors) =>
+                            {
+                                return true;
+                            }
+                        }
+#pragma warning restore CA2000 // Dispose objects before losing scope
+                    )
+                    .CreateLogger();
+
+                
+            }
+            else
+            {
+                Log.Logger = new LoggerConfiguration()
+                    .Enrich.FromLogContext()
+                    .Enrich.WithExceptionDetails()
+                    .WriteTo.Console()
+                    .CreateLogger();
+            }
+            Log.Logger.Information("Document Storage Adapter Container Started");
+            SelfLog.Enable(Console.Error);
+        }
+    }
+}
