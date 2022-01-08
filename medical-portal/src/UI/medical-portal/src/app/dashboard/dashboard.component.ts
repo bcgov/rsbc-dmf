57f64ecfd68761adb821057a09fc9dae1b94586b
George Walker <george@walkersoftware.ca>
2022-01-07 17:41:57 -0800

    S3DMFT-2045 Medical Portal Changes

--- medical-portal/src/UI/medical-portal/src/app/dashboard/dashboard.component.ts
+++ medical-portal/src/UI/medical-portal/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CaseManagementService, DMERCase } from '../shared/services/case-management/case-management.service';
+import {Sort} from '@angular/material/sort';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,6 +11,7 @@ import { CaseManagementService, DMERCase } from '../shared/services/case-managem
 export class DashboardComponent implements OnInit {
 
   public dataSource: DMERCase[] = [];
+  public sortedData: DMERCase[] = [];
   public searchBox: string = '';
 
   constructor(
@@ -18,7 +20,10 @@ export class DashboardComponent implements OnInit {
   ) { }
 
   public ngOnInit(): void {
-    this.caseManagementService.getCases({ byStatus: ['Pending'] }).subscribe(cases => this.dataSource = cases);
+    this.caseManagementService.getCases({ byStatus: ['Pending'] }).subscribe(cases => {
+      this.dataSource = cases;
+      this.sortedData = this.dataSource.slice();
+    } );
   }
 
   public search(): void {
@@ -27,9 +32,45 @@ export class DashboardComponent implements OnInit {
     let searchParams = {
       byTitle: this.searchBox
     };
-    this.caseManagementService.getCases(searchParams).subscribe(cases => this.dataSource = cases);
+    this.caseManagementService.getCases(searchParams).subscribe(cases => {
+      this.dataSource = cases;
+      this.sortedData = this.dataSource.slice();
+    });
 
 
   }
 
+  sortData(sort: Sort) {
+    const data = this.dataSource.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedData = data;
+      return;
+    }
+
+    this.sortedData = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'title':
+          return compare(a.title, b.title, isAsc);
+        case 'patientName':
+          return compare(a.patientName, b.patientName, isAsc);
+        case 'modifiedOn':
+          return compare(a.modifiedOn, b.modifiedOn, isAsc);
+        case 'clinicName':
+          return compare(a.clinicName, b.clinicName, isAsc);
+        case 'status':
+          return compare(a.status, b.status, isAsc);
+        default:
+          return 0;
+      }
+    });
+  }
+}
+
+function compare(a: string | undefined | null, b: number | string | undefined | null, isAsc: boolean) {
+  // check for null or undefined
+  if (a == null || b == null) {
+    return 1;
+  }
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
