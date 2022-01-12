8aa2eed89fca91f3f228e353b08c4512aed796a3
Yossi Tamari <Yossi.Tamari@quartech.com>
2022-01-12 07:51:43 -0800

    updated angular generated api services

--- medical-portal/src/UI/medical-portal/src/app/shared/api/services.ts
+++ medical-portal/src/UI/medical-portal/src/app/shared/api/services.ts
@@ -1,3 +1,4 @@
+export { ApplicationVersionInfoService } from './services/application-version-info.service';
 export { CasesService } from './services/cases.service';
 export { ConfigService } from './services/config.service';
 export { ProfileService } from './services/profile.service';
