e33a5b26efd8b573db0f757696f4949d23e0bfd0
George Walker <george@walkersoftware.ca>
2022-02-13 19:20:36 -0800

    Build fix

--- dps-adapter/src/Pssg.DpsAdapter/Startup.cs
+++ dps-adapter/src/Pssg.DpsAdapter/Startup.cs
@@ -102,19 +102,8 @@ namespace Pssg.IcbcAdapter
 
 
             app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGrpcService<IcbcAdapterService>();
-
-                endpoints.MapGet("/",
-                    async context =>
-                    {
-                        await context.Response.WriteAsync(
-                            "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-                    });
-
-                endpoints.MapControllers();
-
-                
+            {                
+                endpoints.MapControllers();                
             });
 
             // enable Splunk logger using Serilog
