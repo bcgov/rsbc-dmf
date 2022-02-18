1e94f835ac045164d26d9bc63d4c062ba9772c7e
George Walker <george@walkersoftware.ca>
2022-02-17 22:05:39 -0800

    S3DMFT-2348 - Normalization of the Get Driver model

--- icbc-adapter/src/Pssg.IcbcAdapter/Controllers/DriverHistoryController.cs
+++ icbc-adapter/src/Pssg.IcbcAdapter/Controllers/DriverHistoryController.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using Pssg.IcbcAdapter.ViewModels;
 using Pssg.Interfaces;
 using Pssg.Interfaces.Icbc.Models;
+using Pssg.Interfaces.Icbc.ViewModels;
 
 namespace Pssg.IcbcAdapter.Controllers
 {
@@ -34,8 +35,44 @@ namespace Pssg.IcbcAdapter.Controllers
         public ActionResult GetHistory(string driversLicence)
         {
             // get the history from ICBC
-            CLNT result = icbcClient.GetDriverHistory(driversLicence);
-                        
+            CLNT data = icbcClient.GetDriverHistory(driversLicence);
+
+            Driver result = new Driver()
+            {
+                Surname = data.INAM?.SURN,
+                GivenName = data.INAM?.GIV1,                
+                Weight = data.WGHT,
+                Sex = data.SEX,
+                BirthDate = data.BIDT,
+                Height = data.HGHT,
+                SecurityKeyword = data.SECK
+            };
+
+            // handle two middle names, or just one.
+            if (data.INAM?.GIV2 != null && data.INAM?.GIV3 != null)
+            {
+                result.MiddleName = $"{data.INAM.GIV2} {data.INAM.GIV3}";
+            }
+            else if (data.INAM?.GIV2 != null)
+            {
+                result.MiddleName = $"{data.INAM.GIV2}";
+            }
+            else if (data.INAM?.GIV3 != null)
+            {
+                result.MiddleName = $"{data.INAM.GIV3}";
+            }           
+
+            result.DriverMasterStatus = new DriverMasterStatus()
+            {
+                MasterStatusCode = data.DR1MST?.MSCD,
+                RestrictionCodes = new List<int>(), // TODO
+                LicenceExpiryDate = data.DR1MST?.RRDT,
+                LicenceNumber = data.DR1MST?.LNUM,
+                DriverStatus = new DriverStatus(), // TODO
+                LicenceClass = data.DR1MST?.LCLS,
+                DriverMedicals = new List<DriverMedical>() //data.DR1MST?.DR1MEDN,
+            };
+
             return Json (result);
         }
 
