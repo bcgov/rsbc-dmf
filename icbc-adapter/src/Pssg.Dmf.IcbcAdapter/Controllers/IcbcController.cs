3ae94471e71c8f76a8649e9d9c1e1dd5ce78917c
George Walker <george@walkersoftware.ca>
2022-04-05 10:58:31 -0700

    S3DMFT-2689 - OpenAPI spec for ICBC endpoints

---
+++ icbc-adapter/src/Pssg.Dmf.IcbcAdapter/Controllers/IcbcController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Rsbc.Dmf.IcbcAdapter.ViewModels;
+using Pssg.Interfaces;
+using Pssg.Interfaces.Icbc.Models;
+using Pssg.Interfaces.Icbc.ViewModels;
+using Pssg.Interfaces.ViewModelExtensions;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Rsbc.Dmf.IcbcAdapter.Controllers
+{
+
+    public class NewCandidate
+    {
+        /// <summary>
+        /// Driver's License Number
+        /// </summary>
+        public string DlNumber { get; set; }
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Date that the requirement for a Medical Exam was issued
+        /// </summary>
+        DateTimeOffset? EffectiveDate { get; set; }
+
+        /// <summary>
+        /// Birthdate for the Driver
+        /// </summary>
+
+        DateTimeOffset? BirthDate { get; set; }
+
+    }
+
+    public class CaseStatus
+    {
+        public string CaseId { get; set; }
+        public string DlNumber { get; set; }
+        public string Status { get; set; }
+    }
+
+    
+
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class IcbcController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<DriverHistoryController> _logger;
+        private readonly IcbcClient icbcClient;
+
+        public IcbcController(ILogger<DriverHistoryController> logger, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _logger = logger;
+            icbcClient = new IcbcClient(configuration);
+        }
+        
+        /// <summary>
+        /// POST: /Icbc/Candidates
+        /// </summary>
+        /// <param name="newCandidates">List of Candidates to be added to the case management system</param>
+        /// <returns></returns>
+        [HttpPost("Candidates")]
+        [AllowAnonymous]
+        public ActionResult CreateCandidates ([FromBody] List<NewCandidate> newCandidates )
+        {
+            return Ok();
+        }
+
+        [AllowAnonymous]
+        [HttpGet("Cases")]
+        public CaseStatus GetCaseStatus(string caseId)
+        {
+            return new CaseStatus();
+        }
+
+
+
+    }
+}
