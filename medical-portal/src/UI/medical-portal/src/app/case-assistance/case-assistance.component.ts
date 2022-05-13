3553dbe0ec32e1fe26ac0455a216692ef1fdd675
Shruthi Rapala <shruthi.rapala@quartech.com>
2022-05-13 11:38:26 -0700

    2915 - DMER Case Id Assistance page

---
+++ medical-portal/src/UI/medical-portal/src/app/case-assistance/case-assistance.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-case-assistance',
+  templateUrl: './case-assistance.component.html',
+  styleUrls: ['./case-assistance.component.scss']
+})
+export class CaseAssistanceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
