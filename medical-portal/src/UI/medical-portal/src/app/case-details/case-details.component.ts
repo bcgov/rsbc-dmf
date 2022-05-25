91b1b3128d16e0f8ea2c7208931fdcb4b1371a71
Shruthi Rapala <shruthi.rapala@quartech.com>
2022-05-25 10:53:57 -0700

    Added Case DMER Details Page

--- medical-portal/src/UI/medical-portal/src/app/case-details/case-details.component.ts
+++ medical-portal/src/UI/medical-portal/src/app/case-details/case-details.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CaseManagementService, DMERCase } from '../shared/services/case-management/case-management.service';
 
 @Component({
   selector: 'app-case-details',
   templateUrl: './case-details.component.html',
   styleUrls: ['./case-details.component.scss']
 })
-export class CaseDetailsComponent  {
+export class CaseDetailsComponent implements OnInit {
+  selectedCase!: DMERCase;
 
-  constructor() { }
+  constructor(private router: Router, private caseManagementService: CaseManagementService) { }
 
+  public ngOnInit(): void {
+    if (!this.caseManagementService.selectedCase) {
+      this.router.navigate(['dashboard']);
+      return;
+    }
 
-
+    this.selectedCase = this.caseManagementService.selectedCase;
+  }
 }
