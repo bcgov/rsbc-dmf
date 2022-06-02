1f0661f25ebecc13e91bbc1cbc1205d9b8ada9ef
Stanley Okeke <76934070+sookeke@users.noreply.github.com>
2022-06-01 22:07:49 -0700

    Codecov (#1)
    * codecov config for ICBC adapter
    
    * codecov config for ICBC adapter
    
    * codecov config for cms adapter
    
    * Update ci-cms-adapter.yml
    
    * Update ci-cms-adapter.yml
    
    * codecov config for cms adapter
    
    * S3DMFT- Manage Associations with Medical Staff
    - Added table to show medical staff association
    - Added Componnet to create medical staff association
    - Updated Dialog header styles
    
    * S3DMFT- 2799 : Manage Associations of medical staff practitioners
    -  Added component to edit medical staff association
    - Added component to manage/edit medical staff association
    
    * S3DMFT-2799 Manage Associations with Medical Staff
    - Fixed the typo on medical staff association create/edit  dialog
    - Fixed  input parameter for the name
    - Fixed the profile to show correct data on the table rowclick
    
    * S3DMFT-914 - Flat file improvements from testing
    
    * code cov for UI
    
    * code cov for UI
    
    * code cov for UI
    
    * code cov for UI
    
    * code cov for UI
    
    * added ci badge status
    
    * codecov for med portal API
    
    * code cov for UI
    
    * codecov for med portal API
    
    * codecov for med portal API
    
    * codecov for med portal API
    
    * codecov for med portal API
    
    * codecov for med portal API
    
    * codecov for med portal API
    
    * codecov for med portal API
    
    * codecov for med portal API
    
    * codecov for med portal API
    
    * codecov for med portal API
    
    * codecov for med portal API
    
    * codecov for med portal API
    
    * codecov for med portal API
    
    * codecov for med portal API
    
    Co-authored-by: Shruthi Rapala <shruthi.rapala@quartech.com>
    Co-authored-by: George Walker <george@walkersoftware.ca>

--- medical-portal/src/UI/medical-portal/src/app/user-profile/manage-medical-staff-association-dialog/manage-medical-staff-association-dialog.component.ts
+++ medical-portal/src/UI/medical-portal/src/app/user-profile/manage-medical-staff-association-dialog/manage-medical-staff-association-dialog.component.ts
@@ -2,14 +2,11 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-manage-medical-practitioner-association-dialog',
-  templateUrl:
-    './manage-medical-practitioner-association-dialog.component.html',
-  styleUrls: [
-    './manage-medical-practitioner-association-dialog.component.scss',
-  ],
+  selector: 'app-manage-medical-staff-association-dialog',
+  templateUrl: './manage-medical-staff-association-dialog.component.html',
+  styleUrls: ['./manage-medical-staff-association-dialog.component.scss'],
 })
-export class ManageMedicalPractitionerAssociationDialogComponent {
+export class ManageMedicalStaffAssociationDialogComponent {
   displayedColumns: string[] = ['fullName', 'role', 'lastActive'];
   dataSource = [this.data];
 
