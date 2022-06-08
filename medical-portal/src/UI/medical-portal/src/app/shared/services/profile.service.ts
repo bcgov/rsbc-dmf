f2c509dfc15bbb549682214264330d8334c1e55e
Shruthi Rapala <shruthi.rapala@quartech.com>
2022-06-08 15:01:57 -0700

    S3DMFT # 2802 View and manage Medical Practitioner user profile - Updated Edit Medical Practioner user profile to show data - Updated Test data for Medical Practitioner, MOM, MOA tables

--- medical-portal/src/UI/medical-portal/src/app/shared/services/profile.service.ts
+++ medical-portal/src/UI/medical-portal/src/app/shared/services/profile.service.ts
@@ -10,10 +10,23 @@ export class ProfileManagementService {
   constructor(private profileService: ProfileService) { }
 
   public getProfile(params: Parameters<ProfileService['apiProfileCurrentGet$Json']>[0]) {
-    return this.profileService.apiProfileCurrentGet$Json({...params});
+    return this.profileService.apiProfileCurrentGet$Json({...params}).pipe(map((res) => ({...res, businessName: 'Dr. Shelby Drew', licensingInstitution:"College of physicians and surgeons", licenseNumber:"12344555"})));
   }
 
   public updateProfile(params: Parameters<ProfileService['apiProfileEmailPut']>[0]) {
     return this.profileService.apiProfileEmailPut({...params});
   }
+
+  public getProfilePractitionerRoles(params: Parameters<ProfileService['apiProfilePractitionerRolesGet$Response']>[0]) {
+    return this.profileService.apiProfilePractitionerRolesGet$Response({...params}).pipe(map((res) => res.body));
+  }
+
+  public updateProfilePractitionerRoles(params: Parameters<ProfileService['apiProfilePractitionerRolesPut$Response']>[0]) {
+    return this.profileService.apiProfilePractitionerRolesPut$Response({...params}).pipe(map((res) => res.body));;
+  }
+
+  public deleteProfilePractitionerRoles(params: Parameters<ProfileService['apiProfilePractitionerRolesDelete$Response']>[0]) {
+    return this.profileService.apiProfilePractitionerRolesDelete$Response({...params}).pipe(map((res) => res.body));;
+  }
+
 }
