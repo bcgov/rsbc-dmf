8b4e8595616090b648d4a1e3f0aa9df3a15d148c
George Walker <george@walkersoftware.ca>
2022-06-13 13:36:02 -0700

    S3DMFT-2946 - ICBC flat file testing

--- icbc-adapter/src/Pssg.Dmf.IcbcAdapter/Controllers/IcbcController.cs
+++ icbc-adapter/src/Pssg.Dmf.IcbcAdapter/Controllers/IcbcController.cs
@@ -12,6 +12,7 @@ using Pssg.Interfaces.Icbc.Models;
 using Pssg.Interfaces.Icbc.ViewModels;
 using Pssg.Interfaces.ViewModelExtensions;
 using Microsoft.AspNetCore.Authorization;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace Rsbc.Dmf.IcbcAdapter.Controllers
 {
@@ -21,18 +22,21 @@ namespace Rsbc.Dmf.IcbcAdapter.Controllers
         /// <summary>
         /// Driver's License Number
         /// </summary>
+        [SwaggerSchema("Driver's License Number")]
         public string DlNumber { get; set; }
+        [SwaggerSchema("Last Name")]
         public string LastName { get; set; }
 
         /// <summary>
         /// Date that the requirement for a Medical Exam was issued
         /// </summary>
+        [SwaggerSchema("Date that the requirement for a Medical Exam was issued", Format = "date")]
         public DateTimeOffset? EffectiveDate { get; set; }
 
         /// <summary>
         /// Birthdate for the Driver
         /// </summary>
-
+        [SwaggerSchema("Birthdate for the Driver", Format = "date")]
         public DateTimeOffset? BirthDate { get; set; }
 
     }
@@ -68,6 +72,9 @@ namespace Rsbc.Dmf.IcbcAdapter.Controllers
         /// <param name="newCandidates">List of Candidates to be added to the case management system</param>
         /// <returns></returns>
         [HttpPost("Candidates")]
+        [SwaggerResponse(200, "The candidates were processed correctly")]
+        [SwaggerResponse(400, "The format of the provided data was invalid.  Please refer to the model.")]
+        [SwaggerResponse(500, "An unexpected server error occurred while processing. Please retry.")]
         [AllowAnonymous]
         public ActionResult CreateCandidates ([FromBody] List<NewCandidate> newCandidates )
         {
