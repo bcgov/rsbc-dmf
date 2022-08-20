abbb7cc9bad53f8d2b479b2a12ea0f0cceca983b
George Walker <george@walkersoftware.ca>
2022-08-19 22:59:58 -0700

    refactor cms helper

--- icbc-adapter/src/Pssg.Unit.Tests.Interfaces.Icbc/IcbcClientTest.cs
+++ icbc-adapter/src/Pssg.Unit.Tests.Interfaces.Icbc/IcbcClientTest.cs
@@ -9,7 +9,6 @@ using System.Collections.Generic;
 using System.IO;
 using Xunit;
 using Moq;
-using Pssg.Unit.Tests.Interfaces.Icbc.Helpers;
 using Pssg.Interfaces.Icbc.Helpers;
 
 namespace Rsbc.Dmf.IcbcAdapter.Tests
