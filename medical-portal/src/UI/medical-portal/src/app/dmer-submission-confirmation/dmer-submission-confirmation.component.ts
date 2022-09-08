8eac78e24a2ce66d5d141ddfe9caeaa61fe90646
Shruthi Rapala <shruthi.rapala@quartech.com>
2022-09-08 13:28:09 -0700

    S3DMFT# 3550 Medical Portal: Thank you for your submission changes - Added new component for dmer submission confirmation page - Updated UI on the dmer submission page - Added the report  a condition alert toolbar on case details page

---
+++ medical-portal/src/UI/medical-portal/src/app/dmer-submission-confirmation/dmer-submission-confirmation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dmer-submission-confirmation',
+  templateUrl: './dmer-submission-confirmation.component.html',
+  styleUrls: ['./dmer-submission-confirmation.component.scss']
+})
+export class DmerSubmissionConfirmationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
