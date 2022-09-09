55fae2c0705276c260366a1d5ae49ab61bcbd8c7
Shruthi Rapala <shruthi.rapala@quartech.com>
2022-09-09 07:51:41 -0700

    S3DMFT# 3546  Medical Portal: Pop up window feature - Added Dmer submission confirmation modal popup - Added navigation functionality for dashboard and logout buttons

---
+++ medical-portal/src/UI/medical-portal/src/app/dmer-submission-confirmation/dmer-submission-confirmation-dialog/dmer-submission-confirmation-dialog.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DmerSubmissionConfirmationDialogComponent } from './dmer-submission-confirmation-dialog.component';
+
+describe('DmerSubmissionConfirmationDialogComponent', () => {
+  let component: DmerSubmissionConfirmationDialogComponent;
+  let fixture: ComponentFixture<DmerSubmissionConfirmationDialogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DmerSubmissionConfirmationDialogComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DmerSubmissionConfirmationDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
