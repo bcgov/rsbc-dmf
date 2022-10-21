3d1fc611ba0a3cf61351d7fe798768022f47848f
George Walker <george@walkersoftware.ca>
2022-10-21 09:53:09 -0700

    Update TranslateDecisionOutcome

--- cms-adapter/src/Rsbc.Dmf.CaseManagement/CaseManager.cs
+++ cms-adapter/src/Rsbc.Dmf.CaseManagement/CaseManager.cs
@@ -892,6 +892,8 @@ namespace Rsbc.Dmf.CaseManagement
                     .Where(x => x.dfp_decisionid == decisionId).FirstOrDefault();
                 if (d != null && d.dfp_OutcomeStatus != null )
                 {
+                    // ensure the decision has data.
+                    dynamicsContext.LoadPropertyAsync(d.dfp_OutcomeStatus, nameof(dfp_decision.dfp_OutcomeStatus)).GetAwaiter().GetResult();
                     switch (d.dfp_OutcomeStatus.dfp_name)
                     {
                         case "Fit to Drive":
