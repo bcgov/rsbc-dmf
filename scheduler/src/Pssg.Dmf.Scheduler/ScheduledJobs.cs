31ac8ac0b593e133d63be7582b6fc800718f3e01
George Walker <george@walkersoftware.ca>
2022-11-18 11:58:23 -0800

    Skeleton for scheduler

---
+++ scheduler/src/Pssg.Dmf.Scheduler/ScheduledJobs.cs
@@ -0,0 +1,19 @@
+﻿using Hangfire.Server;
+using Hangfire;
+using System.Threading.Tasks;
+
+namespace Rsbc.Dmf.Scheduler
+{
+    public class ScheduledJobs
+    {
+        // add the Hangfire tasks for each job here.
+
+        /// <summary>
+        /// Hangfire job example
+        /// </summary>
+        [AutomaticRetry(Attempts = 0)]
+        public async Task ExampleJob(PerformContext hangfireContext)
+        {
+        }
+    }
+}
