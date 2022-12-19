e0743304fd78b37eb23bfac48f5f74ff89e19e3a
Shruthi Rapala <shruthi.rapala@quartech.com>
2022-12-19 11:33:33 -0800

    Fixed the XML Comment warnings

--- bcmail-adapter/src/Rsbc.Dmf.BcMailAdapter/ViewModels/Case.cs
+++ bcmail-adapter/src/Rsbc.Dmf.BcMailAdapter/ViewModels/Case.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 
 namespace Rsbc.Dmf.BcMailAdapter.ViewModels
 {
+    /// <summary>
+    /// Case View Model
+    /// </summary>
     public class Case
     {
+        /// <summary>
+        /// Case Id
+        /// </summary>
         public string CaseId { get; set; }
 
+        /// <summary>
+        /// Documents
+        /// </summary>
         public List<Document> Documents { get; set; }
     }
 }
