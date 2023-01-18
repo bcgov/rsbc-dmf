14984bb16058eae636b8ffa47898d534a4c22c47
George Walker <george@walkersoftware.ca>
2023-01-18 14:57:17 -0800

    S3DMFT-4159 Improvements to legacy add document

---
+++ legacy-adapter/src/Rsbc.Dmf.LegacyAdapter/DocumentUtils.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace Rsbc.Dmf.LegacyAdapter
+{
+    public static class DocumentUtils
+    {
+
+        /// <summary>
+        /// SanitizeKeyFilename
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        static public string SanitizeKeyFilename(string data)
+        {
+
+            var invalidCharacters = Path.GetInvalidFileNameChars().ToList();
+            invalidCharacters.Add(' ');
+            invalidCharacters.Add('/');
+            invalidCharacters.Add('\\');
+
+            string result = new string(data
+                .Where(x => !invalidCharacters.Contains(x))
+                .ToArray());
+
+            return result;
+        }
+
+        /// <summary>
+        /// GetMimeType
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        static public string GetMimeType(string filename)
+        {
+            string mimetype = "application/pdf";
+            if (!string.IsNullOrEmpty(filename))
+            {
+                string extension = Path.GetExtension(filename);
+
+                if (extension != null && (".tif" == extension.ToLower() || ".tiff" == extension.ToLower()))
+                {
+                    mimetype = "image/tiff";
+                }
+            }
+
+            return mimetype;
+        }
+    }
+}
