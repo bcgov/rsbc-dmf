1a255a2606a37346bf56953dc8cdd1021748f3d4
Shruthi Rapala <shruthi.rapala@quartech.com>
2023-02-13 13:39:26 -0800

    S3DMFT# BC Mail service development in OpenShift Refactor BCMail Adapter to use the CDGS Client - Added CDGS Client Project - Added CDGS Models - Added Cdgs Client

---
+++ bcmail-adapter/src/Rsbc.Interfaces.Dmf.BcMailAdapter/ICdgsClient.cs
@@ -0,0 +1,15 @@
+﻿using Rsbc.Interfaces.CdgsModels;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Rsbc.Interfaces
+{
+    public interface ICdgsClient
+    {
+       /* public CLNT GetDriverHistory(string dlNumber);
+
+        public string SendMedicalUpdate(IcbcMedicalUpdate item);*/
+
+        public Task<Stream> PreviewBcMailDocument(LetterGenerationRequest request);
+    }
+}
