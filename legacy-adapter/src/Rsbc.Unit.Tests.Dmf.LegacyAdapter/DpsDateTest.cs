be2d8a53461b15c47799b00ff2a8d88924641967
George Walker <george@walkersoftware.ca>
2023-02-15 14:33:21 -0800

    S3DMFT-4418 - fix for problems with DPS date formats

---
+++ legacy-adapter/src/Rsbc.Unit.Tests.Dmf.LegacyAdapter/DpsDateTest.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Net.Client;
+using Microsoft.Extensions.Configuration;
+using Pssg.Interfaces.Icbc.Helpers;
+using Pssg.Interfaces.Icbc.Models;
+using Pssg.Interfaces;
+using Rsbc.Dmf.CaseManagement.Helpers;
+using Rsbc.Dmf.CaseManagement.Service;
+using Rsbc.Dmf.LegacyAdapter;
+using System.Net.Http;
+using System;
+using Xunit;
+using System.Net;
+using Rsbc.Dmf.LegacyAdapter.ViewModels;
+
+namespace Rsbc.Unit.Tests.Dmf.LegacyAdapter
+{
+    
+
+    public class DpsDateTest
+
+    {
+
+        
+
+        [Fact]
+        public void TestNearMidnight()
+        {
+            string input = "2023-02-09T0:20:07Z";
+            DateTimeOffset dto = DocumentUtils.ParseDpsDate(input);
+
+            Assert.True(dto.Day == 9);
+        }
+
+
+    }
+}
