5325c1680b49f89788f7a177d677d55de4706e13
Shruthi Rapala <shruthi.rapala@quartech.com>
2023-02-21 07:45:23 -0800

    S3DMFT # 4429 Adding Headers and footer to the letter generation document - Added Create document method to add headers and footers to PDF - Updated attachment model to include Headers and footers - Added MArigold.XHTML Nuget package to combine headers and footers

--- bcmail-adapter/src/Rsbc.Dmf.BcMailAdapter/ViewModels/Attachment.cs
+++ bcmail-adapter/src/Rsbc.Dmf.BcMailAdapter/ViewModels/Attachment.cs
@@ -11,6 +11,16 @@
         public string? Body { get; set; }
 
         /// <summary>
+        /// Header
+        /// </summary>
+        public string? Header { get; set; }
+
+        /// <summary>
+        /// Footer
+        /// </summary>
+        public string? Footer { get; set; }
+
+        /// <summary>
         /// Content Type
         /// </summary>
         public string? ContentType { get; set; }
--- bcmail-adapter/src/Rsbc.Unit.Tests.Dmf.BcMailAdapter/CdgsClientTest.cs
+++ bcmail-adapter/src/Rsbc.Unit.Tests.Dmf.BcMailAdapter/CdgsClientTest.cs
@@ -62,5 +62,43 @@ namespace Rsbc.Dmf.BcMailAdapter.Tests
             
         }
 
+        /// <summary>
+        /// Test the CDGS Client
+        /// </summary>
+        [Fact]
+        public async void TestDocxCdgsClient()
+        {
+
+            if (!string.IsNullOrEmpty(_cdgsServiceUri))
+            {
+
+                // construct docx using marigold library convert that to docx and pass it to cdogs
+                // var docx = this.CreateDocument(attachment.Body, attachment.Header, attachment.Footer);
+
+                string testString = "test";
+                var request = new HttpRequestMessage(HttpMethod.Post, "template/render");
+
+                var result = new PdfResponse()
+                {
+                    FileName = "Test BC mail Preview",
+                    ContentType = "text/plain",
+                    // 
+                    Body = Convert.ToBase64String(Encoding.ASCII.GetBytes(testString)),
+                };
+
+                string jsonString = JsonConvert.SerializeObject(result);
+                request.Content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+                var response = _client.SendAsync(request).GetAwaiter().GetResult();
+
+                response.EnsureSuccessStatusCode();
+
+                // parse as JSON.
+                jsonString = await response.Content.ReadAsStringAsync();
+                BcMail bcMail = JsonConvert.DeserializeObject<BcMail>(jsonString);
+
+                Assert.Equal(Convert.FromBase64String(bcMail.Attachments[0].Body), Encoding.ASCII.GetBytes(testString));
+            }
+
+        }
     }
 }
