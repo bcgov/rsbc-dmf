05806ce2926ed83df68b74bc7097ceec5d477a51
Shruthi Rapala <shruthi.rapala@quartech.com>
2023-02-23 09:36:53 -0800

    S3DMFT # 3740 Convert TIFF to PDF in Mercury -  Added a feature flag to verift the tiff document type - Updated the Document end point to convert tiff to pdf -Added Document Convert Util file - Added unit tests for testing tiff to pdf conversion

---
+++ document-storage-adapter/src/Pssg.Unit.Tests.Interfaces.S3/DocumentConvertUtilTest.cs
@@ -0,0 +1,29 @@
+﻿using Pssg.DocumentStorageAdapter;
+using System.IO;
+using Xunit;
+
+namespace Pssg.Unit.Tests.Interfaces.S3
+{
+    public class DocumentConvertUtilTest
+    {
+        /// <summary>
+        /// Convert Tiff 2Pdf Test
+        /// </summary>
+        [Fact]
+        public void ConvertTiff2PdfTest()
+        {
+            //Arrange
+            // Get TiffImage as bytes array
+            byte[] imageAsByteStream = File.ReadAllBytes("../../../TestFiles/test_file_1.tiff");
+
+            // Act 
+            // and then send it to convertTiff2Pdf function 
+            var pdfBytes = DocumentConvertUtil.convertTiff2Pdf(imageAsByteStream);
+
+            // Asset 
+            // check result is of type bytes 
+            // check result bytes length  
+            Assert.True(pdfBytes.Length > 0);
+        }
+    }
+}
