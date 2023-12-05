873049ef7e884d8bfa5e11ef368f60fe01d390e0
Shruthi Rapala <shruthi.rapala@quartech.com>
2023-12-05 10:53:37 -0800

    DFTDP # 66 Account Creation for driver portal - Fixed the issue with headers and footer, Added font and images to driver portal Added Navigation bar to driver portal

--- driver-portal/src/ClientApp/src/app/app.component.ts
+++ driver-portal/src/ClientApp/src/app/app.component.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
 import { Component, Inject, OnInit } from '@angular/core';
 import { LoginService } from './shared/services/login.service';
 import { ConfigurationService } from './shared/services/configuration.service';
@@ -23,28 +24,28 @@ export class AppComponent implements OnInit {
   ) { }
 
   public async ngOnInit(): Promise<void> {
-    try {
-      //load the configuration from the server
-      await firstValueFrom(this.configService.load());
-      //attempt to log in
-      let nextRoute = await firstValueFrom(this.loginService.login(location.pathname.substring(1) || 'dashboard'));
+    //try {
+    //  //load the configuration from the server
+    //  await firstValueFrom(this.configService.load());
+    //  //attempt to log in
+    //  let nextRoute = await firstValueFrom(this.loginService.login(location.pathname.substring(1) || 'dashboard'));
 
-      //get the user's profile
-      await firstValueFrom(this.loginService.getUserProfile());
+    //  //get the user's profile
+    //  await firstValueFrom(this.loginService.getUserProfile());
 
-      //determine the next route
-      nextRoute = '/' + decodeURIComponent(nextRoute);
+    //  //determine the next route
+    //  nextRoute = '/' + decodeURIComponent(nextRoute);
 
-      // remove base path.
-      if (this.baseHref && nextRoute.substring(0, this.baseHref.length) === this.baseHref) {
-        nextRoute = nextRoute.substring(this.baseHref.length);
-      }
+    //  // remove base path.
+    //  if (this.baseHref && nextRoute.substring(0, this.baseHref.length) === this.baseHref) {
+    //    nextRoute = nextRoute.substring(this.baseHref.length);
+    //  }
 
-      this.router.navigate([nextRoute]).then(() => this.isLoading = false)
-    } catch (e) {
-      console.error(e);
-      throw e;
-    }
+    //  this.router.navigate([nextRoute]).then(() => this.isLoading = false)
+    //} catch (e) {
+    //  console.error(e);
+    //  throw e;
+    //}
   }
 
   public showNavigation(): boolean {
