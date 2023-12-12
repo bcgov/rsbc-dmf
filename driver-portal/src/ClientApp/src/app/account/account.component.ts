4a4c1ad18fae2c60fbc9fdd5f9118cbcb300ab60
Shruthi Rapala <shruthi.rapala@quartech.com>
2023-12-12 10:20:56 -0800

    DFTDP # 69 Account Creation - Select Notification Preference - Updated styles on the account page. - Adjusted selected notifications section - Adjusted personal information section

--- driver-portal/src/ClientApp/src/app/account/account.component.ts
+++ driver-portal/src/ClientApp/src/app/account/account.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.scss']
 })
 export class AccountComponent {
 
