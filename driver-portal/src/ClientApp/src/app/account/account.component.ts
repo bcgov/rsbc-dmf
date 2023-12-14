22aefe9f19b5fc9109b6c26b1049ea5f16f8d1cf
Shruthi Rapala <shruthi.rapala@quartech.com>
2023-12-14 14:05:53 -0800

    DFTDP # 70 Account Creation - Update Address - Added UI functionality for showing the update address and phone number

--- driver-portal/src/ClientApp/src/app/account/account.component.ts
+++ driver-portal/src/ClientApp/src/app/account/account.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.scss']
+  styleUrls: ['./account.component.scss'],
 })
-export class AccountComponent {
+export class AccountComponent implements OnInit {
+  isEditView = false;
+  setEmailAddress = true;
+  
+  accountForm = this.fb.group({
+    mail: [false]
+  })
 
-  setEmailAddress = true
-  onEdit(){
-      this.setEmailAddress = false;
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.accountForm.valueChanges.subscribe((val) => {
+      console.log(val);
+    })
   }
 
+  onEdit() {
+    this.setEmailAddress = false;
+    this.isEditView = true;
+  }
 }
