6d40288f91ca63bf6f4aff33b6d6ec254d90adc4
Jebb Burditt <Jebb.Burditt@quartech.com>
2023-12-20 15:30:57 -0800

    case manager tests 2

---
+++ cms-adapter/src/Rsbc.Dmf.CaseManagement.Tests/Integration/CaseManagerTests2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Rsbc.Dmf.CaseManagement.Tests.Integration
+{
+    /// <summary>
+    /// CaseManagerTests using XUnit dependency injection
+    /// </summary>
+    public class CaseManagerTests2
+    {
+        private readonly ICaseManager _caseManager;
+        private readonly IConfiguration _configuration;
+
+        public CaseManagerTests2(ICaseManager caseManager, IConfiguration configuration)
+        {
+            _caseManager = caseManager;
+            _configuration = configuration;
+        }
+
+        [Fact]
+        public async Task CanGetCaseDetails()
+        {
+            var caseId = _configuration["ICBC_TEST_CASEID"];
+            var c = await _caseManager.GetCaseDetail(caseId);
+            Assert.NotNull(c);
+        }
+    }
+}
