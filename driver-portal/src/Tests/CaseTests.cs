c7568cac3d774a000c65b12383cfc3e71e0f3ab7
Jebb Burditt <Jebb.Burditt@quartech.com>
2023-12-21 15:55:09 -0800

    finish get case integration test

--- driver-portal/src/Tests/CaseTests.cs
+++ driver-portal/src/Tests/CaseTests.cs
@@ -1,6 +1,5 @@
 using Newtonsoft.Json;
 using Rsbc.Dmf.DriverPortal.ViewModels;
-using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
@@ -15,20 +14,15 @@ namespace Rsbc.Dmf.DriverPortal.Tests
         [Fact]
         public async Task GetCase()
         {
-            // TODO move to configuration
-            var caseId = "aa937e8f-171b-ec11-b82d-00505683fbf4";
-            var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost:8080/api/Cases/GetCase/" + caseId);
+            var caseId = Configuration["ICBC_TEST_CASEID"];
+            var request = new HttpRequestMessage(HttpMethod.Get, "/api/Cases/GetCase/" + caseId);
 
-            var response = _client.SendAsync(request).GetAwaiter().GetResult();
-
-            // parse as JSON.
+            var response = await _client.SendAsync(request);
             var jsonString = await response.Content.ReadAsStringAsync();
-
             response.EnsureSuccessStatusCode();
 
-            CaseDetail clientResult = JsonConvert.DeserializeObject<CaseDetail>(jsonString);
+            var clientResult = JsonConvert.DeserializeObject<CaseDetail>(jsonString);
 
-            // content should match
             Assert.Equal(clientResult.CaseId, caseId);
         }
     }
