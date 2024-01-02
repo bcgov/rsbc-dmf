283e84d3e279d8908800ece0c157b0b08e619098
Jebb Burditt <Jebb.Burditt@quartech.com>
2024-01-02 06:39:24 -0800

    include the deleted lines

--- driver-portal/src/Tests/CaseTests.cs
+++ driver-portal/src/Tests/CaseTests.cs
@@ -13,7 +13,7 @@ namespace Rsbc.Dmf.DriverPortal.Tests
         [Fact]
         public async Task GetCase()
         {
-            var caseId = Configuration["ICBC_TEST_CASEID"];
+
             var caseId = _configuration["ICBC_TEST_CASEID"];
             var request = new HttpRequestMessage(HttpMethod.Get, $"{CASE_API_BASE}/" + caseId);
 
@@ -25,7 +25,7 @@ namespace Rsbc.Dmf.DriverPortal.Tests
         [Fact]
         public async Task GetMostRecentCase()
         {
-            var licenseNumber = Configuration["ICBC_TEST_DL"];
+
             //var licenseNumber = _configuration["ICBC_TEST_DL"];
             var request = new HttpRequestMessage(HttpMethod.Get, $"{CASE_API_BASE}/MostRecent/");
 
