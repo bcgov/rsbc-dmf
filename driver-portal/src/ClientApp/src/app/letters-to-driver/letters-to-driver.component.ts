63e94caadef2b4310462b7ef712d22c1ae8b2b04
Shruthi Rapala <shruthi.rapala@quartech.com>
2024-01-11 15:17:21 -0800

    DFTDP # 104 UI Development for case details page - Optimized the calls for case details component
    DFTDP # 103 Implement UI for closed cases
    - Added api ed point to get the closed cases
    - Added UI for the closed cases

--- driver-portal/src/ClientApp/src/app/letters-to-driver/letters-to-driver.component.ts
+++ driver-portal/src/ClientApp/src/app/letters-to-driver/letters-to-driver.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { MatAccordion } from '@angular/material/expansion';
 import { CaseManagementService } from '../shared/services/case-management/case-management.service';
 import { Document } from '../shared/api/models';
@@ -8,25 +8,8 @@ import { Document } from '../shared/api/models';
   templateUrl: './letters-to-driver.component.html',
   styleUrls: ['./letters-to-driver.component.css'],
 })
-export class LettersToDriverComponent implements OnInit {
+export class LettersToDriverComponent {
+  @Input() letterDocuments?: Document[] | null = [];
+
   @ViewChild(MatAccordion) accordion!: MatAccordion;
-
-  public letterDocuments: Document[] = [];
-  constructor(private caseManagementService: CaseManagementService) {}
-
-  ngOnInit(): void {
-    this.getLetterDocuments('e27d7c69-3913-4116-a360-f5e990200173');
-  }
-
-  getLetterDocuments(driverId: string) {
-    this.caseManagementService
-      .getDriverDocuments({ driverId })
-      .subscribe((letters: any) => {
-        if (letters?.lettersToDriver && letters?.lettersToDriver?.length > 0) {
-          this.letterDocuments = letters.lettersToDriver;
-        }
-
-        console.log(letters);
-      });
-  }
 }
