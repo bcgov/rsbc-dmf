595b34b59261875d9cea4222192a7e456b87dd7e
Shruthi Rapala <shruthi.rapala@quartech.com>
2024-01-15 14:04:49 -0800

    DFTHG # 96 UI Development for Dashboard. - Added the account button to the header to navigate to account page - Added functionality to logout.

--- shared-portal-ui/projects/core-ui/src/lib/layout/layout.module.ts
+++ shared-portal-ui/projects/core-ui/src/lib/layout/layout.module.ts
@@ -3,25 +3,21 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import {RouterModule, Router} from '@angular/router'
+import { RouterModule, Router } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    FooterComponent,
-    NavMenuComponent,
-    ],
+  declarations: [HeaderComponent, FooterComponent, NavMenuComponent],
   imports: [
     CommonModule,
     RouterModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
   ],
-  exports:[
-    HeaderComponent,
-    FooterComponent,
-    NavMenuComponent,
-  ],
-  schemas: [
-    CUSTOM_ELEMENTS_SCHEMA
-  ],
+  exports: [HeaderComponent, FooterComponent, NavMenuComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class LayoutModule { }
+export class LayoutModule {}
