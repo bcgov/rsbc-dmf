1aceca3e51bd9939cffe1ed18a72facfd2716810
Jebb Burditt <Jebb.Burditt@quartech.com>
2024-01-23 16:15:35 -0800

    undo change to api-configuration that sneaked in

--- driver-portal/src/ClientApp/src/app/shared/api/api-configuration.ts
+++ driver-portal/src/ClientApp/src/app/shared/api/api-configuration.ts
@@ -9,7 +9,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiConfiguration {
-  rootUrl: string = 'http://localhost:8080';
+  rootUrl: string = '';
 }
 
 /**
