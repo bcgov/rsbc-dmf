5f00da09e02d227b04fe1adc5ccbc7bd44c632e7
Jebb Burditt <Jebb.Burditt@quartech.com>
2024-01-24 08:57:57 -0800

    why???

--- driver-portal/src/Model/Attribute/AuthorizeDriverAttribute.cs
+++ driver-portal/src/Model/Attribute/AuthorizeDriverAttribute.cs
@@ -1,9 +1,13 @@
-﻿namespace Rsbc.Dmf.DriverPortal.Api
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Rsbc.Dmf.DriverPortal.Api
 {
     // TODO remove this without github build error
     // NOTE removing this and building locally works, I tried cleaning the github build and building dockers
     public class AuthorizeDriverAttribute
     {
-      
+        public class AuthorizeDriver
+        {
+        }
     }
 }
