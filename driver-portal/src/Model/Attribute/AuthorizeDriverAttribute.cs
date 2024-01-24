5db1bf5b9525c72c59ec2e408027638232d3c9b1
Jebb Burditt <Jebb.Burditt@quartech.com>
2024-01-24 08:51:23 -0800

    hacky but I'm out of ideas

---
+++ driver-portal/src/Model/Attribute/AuthorizeDriverAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace Rsbc.Dmf.DriverPortal.Api
+{
+    // TODO remove this without github build error
+    // NOTE removing this and building locally works, I tried cleaning the github build and building dockers
+    public class AuthorizeDriverAttribute
+    {
+      
+    }
+}
