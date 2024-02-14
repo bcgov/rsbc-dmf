03fc0392108d303c72616458a0840d79ddf603cb
Jebb Burditt <Jebb.Burditt@quartech.com>
2024-02-14 12:03:15 -0800

    remove okresults

--- driver-portal/src/ClientApp/src/app/shared/api/models.ts
+++ driver-portal/src/ClientApp/src/app/shared/api/models.ts
@@ -9,6 +9,5 @@ export { Document } from './models/document';
 export { Driver } from './models/driver';
 export { EmailUpdate } from './models/email-update';
 export { OidcOptions } from './models/oidc-options';
-export { OkResult } from './models/ok-result';
 export { ProblemDetails } from './models/problem-details';
 export { UserProfile } from './models/user-profile';
