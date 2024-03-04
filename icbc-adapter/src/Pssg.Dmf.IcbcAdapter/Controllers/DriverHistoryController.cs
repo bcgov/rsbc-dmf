c692d95c7a8ca4e7023a28225c3b96a4c84ed9be
Shruthi Rapala <shruthi.rapala@quartech.com>
2024-03-04 10:08:35 -0800

    PO Box being inserted into the middle of the Street Address Updated POBOX address  issue to show that to bottom of the address line

--- icbc-adapter/src/Pssg.Dmf.IcbcAdapter/Controllers/DriverHistoryController.cs
+++ icbc-adapter/src/Pssg.Dmf.IcbcAdapter/Controllers/DriverHistoryController.cs
@@ -166,7 +166,7 @@ namespace Rsbc.Dmf.IcbcAdapter.Controllers
 
                     if (!string.IsNullOrEmpty(data.ADDR.POBX))
                     {
-                        addressComponents += $"\n PO BOX {data.ADDR.POBX}";
+                        addressComponents += $"\n</br> PO BOX {data.ADDR.POBX}";
                     }
 
                     result.AddressLine1 = addressComponents;
