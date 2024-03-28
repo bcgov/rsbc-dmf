44199a2a19459a9a5d431256b5d32632d274325f
Jebb Burditt <jebb.burditt@gmail.com>
2024-03-28 15:56:30 -0700

    tested one login working but missing enable icbc configuration

--- driver-portal/src/Controllers/ProfileController.cs
+++ driver-portal/src/Controllers/ProfileController.cs
@@ -3,8 +3,10 @@ using Microsoft.AspNetCore.Mvc;
 using Rsbc.Dmf.CaseManagement.Service;
 using Rsbc.Dmf.DriverPortal.Api.Services;
 using Rsbc.Dmf.DriverPortal.ViewModels;
+using Rsbc.Dmf.IcbcAdapter;
 using System.Net;
 using System.Security.Claims;
+using static Rsbc.Dmf.IcbcAdapter.IcbcAdapter;
 
 namespace Rsbc.Dmf.DriverPortal.Api.Controllers
 {
@@ -16,14 +18,16 @@ namespace Rsbc.Dmf.DriverPortal.Api.Controllers
     {
         private readonly CaseManager.CaseManagerClient _cmsAdapterClient;
         private readonly UserManager.UserManagerClient _userManagerClient;
-        private readonly IUserService userService;
+        private readonly IcbcAdapterClient _icbcAdapterClient;
+        private readonly IUserService _userService;
         private readonly ILogger<ProfileController> _logger;
         private readonly IConfiguration _configuration;
 
-        public ProfileController(CaseManager.CaseManagerClient cmsAdapterClient, UserManager.UserManagerClient userManagerClient, IUserService userService, ILoggerFactory loggerFactory, IConfiguration configuration)
+        public ProfileController(CaseManager.CaseManagerClient cmsAdapterClient, UserManager.UserManagerClient userManagerClient, IcbcAdapterClient icbcAdapterClient, IUserService userService, ILoggerFactory loggerFactory, IConfiguration configuration)
         {
-            this.userService = userService;
+            _userService = userService;
             _cmsAdapterClient = cmsAdapterClient;
+            _icbcAdapterClient = icbcAdapterClient;
             _configuration = configuration;
             _userManagerClient = userManagerClient;
             _logger = loggerFactory.CreateLogger<ProfileController>();
@@ -36,7 +40,7 @@ namespace Rsbc.Dmf.DriverPortal.Api.Controllers
         [ActionName(nameof(GetCurrentProfile))]
         public async Task<ActionResult<UserProfile>> GetCurrentProfile()
         {
-            var userContext = await userService.GetCurrentUserContext();
+            var userContext = await _userService.GetCurrentUserContext();
             if (userContext == null) 
             { 
                 return NotFound(""); 
@@ -51,7 +55,7 @@ namespace Rsbc.Dmf.DriverPortal.Api.Controllers
                 var driverIdRequest = new DriverIdRequest() { Id = userContext.DriverId };
                 var reply = _cmsAdapterClient.GetDriverById(driverIdRequest);
 
-                if (reply.ResultStatus == ResultStatus.Success && reply.Items != null && reply.Items.Count > 0)
+                if (reply.ResultStatus == CaseManagement.Service.ResultStatus.Success && reply.Items != null && reply.Items.Count > 0)
                 {
                     var driverRecord = reply.Items.FirstOrDefault();
                     if (driverRecord != null)
@@ -86,17 +90,44 @@ namespace Rsbc.Dmf.DriverPortal.Api.Controllers
         [ActionName(nameof(Register))]
         public async Task<ActionResult> Register([FromBody] UserRegistration userRegistration)
         {
-            var profile = await userService.GetCurrentUserContext();
+            var profile = await _userService.GetCurrentUserContext();
             if (profile == null) 
             { 
                 return NotFound(); 
             }
 
-            // verify user registration name and birth date
+            var driverInfoRequest = new DriverInfoRequest();
+            driverInfoRequest.DriverLicence = userRegistration.DriverLicenseNumber;
+
+            DriverInfoReply reply = null;
+            // TODO add caching
+            //if (!_cacheService.TryGetValue(nameof(IcbcAdapterClient.GetDriverInfo), driverInfoRequest.DriverLicence, out reply))
+            //{
+                        reply = _icbcAdapterClient.GetDriverInfo(driverInfoRequest);
+            //}
+
+            // TODO add extension method string.ToDateTime()
+            // security validation
+            if (!DateTime.TryParse(profile.BirthDate, out DateTime claimBirthDate))
+            {
+                _logger.LogError($"{nameof(Register)} could not parse profile birthdate.");
+                return StatusCode((int)HttpStatusCode.Unauthorized, "No driver found.");
+            }
+            if (!DateTime.TryParse(reply.BirthDate, out DateTime replyBirthDate))
+            {
+                _logger.LogError($"{nameof(Register)} could not parse ICBC birthdate.");
+                return StatusCode((int)HttpStatusCode.Unauthorized, "No driver found.");
+            }
+            if (profile.FirstName != reply.GivenName || profile.LastName != reply.Surname || claimBirthDate.Date != replyBirthDate.Date)
+            {
+                return StatusCode((int)HttpStatusCode.Unauthorized);
+            }
+
+            // NOTE this will create the driver, if it does not exist
             var driverLicenseRequest = new DriverLicenseRequest();
             driverLicenseRequest.DriverLicenseNumber = userRegistration.DriverLicenseNumber;
             var getDriverReply = _cmsAdapterClient.GetDriverPerson(driverLicenseRequest);
-            if (getDriverReply.ResultStatus != ResultStatus.Success)
+            if (getDriverReply.ResultStatus != CaseManagement.Service.ResultStatus.Success)
             {
                 _logger.LogError($"{nameof(Register)} failed.\n {0}", getDriverReply.ErrorDetail);
                 return StatusCode((int)HttpStatusCode.InternalServerError, getDriverReply.ErrorDetail);
@@ -107,43 +138,13 @@ namespace Rsbc.Dmf.DriverPortal.Api.Controllers
             }
             CaseManagement.Service.Driver foundDriver = null;
 
-            // parse birthdate
-            if (!DateTime.TryParse(profile.BirthDate, out DateTime claimBirthDate))
-            {
-                _logger.LogError($"{nameof(Register)} could not parse DL.");
-                return StatusCode((int)HttpStatusCode.Unauthorized, "No driver found.");
-            }
-
-            foreach (var driver in getDriverReply.Items)
-            {
-                if (string.IsNullOrEmpty(_configuration["ENABLE_ICBC"]))
-                {
-                    foundDriver = driver;
-                }
-                else
-                {
-                    DateTime driverBirthDate = new DateTime(driver.BirthDate.ToDateTimeOffset().Year,
-                        driver.BirthDate.ToDateTimeOffset().Month, driver.BirthDate.ToDateTimeOffset().Day);
-
-                    if (profile.FirstName == driver.GivenName && profile.LastName == driver.Surname && claimBirthDate.Date == driver.BirthDate.ToDateTime().Date)
-                    {
-                        foundDriver = driver;
-                    }
-                }
-                    
-            }
-            if (foundDriver == null)
-            {
-                return StatusCode((int)HttpStatusCode.Unauthorized, "Driver details do not match.");
-            }
-
             // update driver email
             var request = new SetDriverLoginRequest();
             request.LoginId = profile.Id;
-            request.DriverId = foundDriver.Id;
+            request.DriverId = getDriverReply.Items.First().Id;
 
             var setDriverReply = _userManagerClient.SetDriverLogin(request);
-            if (setDriverReply.ResultStatus != ResultStatus.Success) 
+            if (setDriverReply.ResultStatus != CaseManagement.Service.ResultStatus.Success) 
             {
                 _logger.LogError($"{nameof(Register)}.{nameof(UserManager.UserManagerClient.SetDriverLogin)} failed.\n {0}", setDriverReply.ErrorDetail);
                 return StatusCode((int)HttpStatusCode.InternalServerError, setDriverReply.ErrorDetail);
@@ -157,15 +158,15 @@ namespace Rsbc.Dmf.DriverPortal.Api.Controllers
             updateLoginRequest.NotifyByEmail = userRegistration.NotifyByEmail;
             updateLoginRequest.ExternalUserName = profile.DisplayName;
             updateLoginRequest.Address = userRegistration.Address;
-
             setDriverReply = _userManagerClient.UpdateLogin(updateLoginRequest);
-            if (setDriverReply.ResultStatus != ResultStatus.Success)
+            if (setDriverReply.ResultStatus != CaseManagement.Service.ResultStatus.Success)
             {
                 _logger.LogError($"{nameof(UserRegistration)}.{nameof(UserManager.UserManagerClient.UpdateLogin)} failed.\n {0}", setDriverReply.ErrorDetail);
                 return StatusCode((int)HttpStatusCode.InternalServerError, setDriverReply.ErrorDetail);
             }
 
-            userService.UpdateClaim(ClaimTypes.Email, userRegistration.Email);
+            // TODO we probably need to update driver id and driver license number if a new driver was added above
+            _userService.UpdateClaim(ClaimTypes.Email, userRegistration.Email);
 
             return Ok();
         }
@@ -179,7 +180,7 @@ namespace Rsbc.Dmf.DriverPortal.Api.Controllers
         [ActionName(nameof(UpdateDriver))]
         public async Task<ActionResult> UpdateDriver([FromBody] DriverUpdate request)
         {
-            var profile = await userService.GetCurrentUserContext();
+            var profile = await _userService.GetCurrentUserContext();
             if (profile == null) 
             { 
                 return NotFound(); 
@@ -189,13 +190,13 @@ namespace Rsbc.Dmf.DriverPortal.Api.Controllers
             updateEmailRequest.Email = request.Email;
             updateEmailRequest.LoginId = profile.Id;
             var reply = _userManagerClient.UpdateEmail(updateEmailRequest);
-            if (reply.ResultStatus != ResultStatus.Success)
+            if (reply.ResultStatus != CaseManagement.Service.ResultStatus.Success)
             {
                 _logger.LogError($"{nameof(UpdateDriver)}.{nameof(UserManager.UserManagerClient.UpdateEmail)} failed.\n {0}", reply.ErrorDetail);
                 return StatusCode((int)HttpStatusCode.InternalServerError, reply.ErrorDetail);
             }
 
-            userService.UpdateClaim(ClaimTypes.Email, request.Email);
+            _userService.UpdateClaim(ClaimTypes.Email, request.Email);
 
             return Ok();
         }
