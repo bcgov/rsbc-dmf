0366b88571f82294b2724bfc78adb7a6f1e46271
Shruthi Rapala <shruthi.rapala@quartech.com>
2024-04-02 16:34:38 -0700

    DFTDP # 117 Create new project for Medical Portal - Added header, footer, nav bar and dashboard componets - Added styles and images - Added angular material packages

--- medical-practitioner-portal/src/app/app.component.ts
+++ medical-practitioner-portal/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './Layout/header/header.component';
+import { FooterComponent } from './Layout/footer/footer.component';
+import { NavMenuComponent } from './Layout/nav-menu/nav-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, NavMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
