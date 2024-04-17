b1b10c1d212365700094ff0428045cf48a4ca8fc
Jebb Burditt <jebb.burditt@gmail.com>
2024-04-17 10:23:01 -0700

    add web server

--- medical-portal/src/PidpAdpater.API/PidpAdpater.API/Program.cs
+++ medical-portal/src/PidpAdpater.API/PidpAdpater.API/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//using HealthChecks.UI.Client;
+//using IdentityModel.AspNetCore.OAuth2Introspection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.IdentityModel.Tokens;
+//using Microsoft.OpenApi.Models;
+using System.Net;
+using System.Security.Claims;
+using System.Text.Json.Serialization;
+
+Console.WriteLine($"Starting Pidp Adapter.");
+
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost
+    .UseUrls()
+    .UseKestrel(options => { options.Listen(IPAddress.Any, 7215); });
+
+// add services to DI container
+var services = builder.Services;
+var env = builder.Environment;
