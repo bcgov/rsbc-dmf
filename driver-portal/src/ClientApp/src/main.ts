d158d1477ab7ec00a7c5c2ad7e44168a0d24f8e8
shruthi-rapala <shruthi.rapala@quartech.com>
2024-04-26 13:26:31 -0700

    Fixed Lint errors in main.ts

--- driver-portal/src/ClientApp/src/main.ts
+++ driver-portal/src/ClientApp/src/main.ts
@@ -1,7 +1,4 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
 import { ApiModule } from './app/shared/api/api.module';
@@ -19,9 +16,9 @@ export function getBaseUrl() {
   return document.getElementsByTagName('base')[0].href;
 }
 
-const providers = [
-  { provide: 'BASE_URL', useFactory: getBaseUrl, deps: [] }
-];
+// const providers = [
+//   { provide: 'BASE_URL', useFactory: getBaseUrl, deps: [] }
+// ];
 
 if (environment.production) {
   enableProdMode();
