fc467a810a3407c04fa4575b1d58dc6aa929d82d
Jebb Burditt <jebb.burditt@gmail.com>
2024-04-29 15:22:20 -0700

    refactor frontend and backend to use latest format by VS

--- driver-portal/src/driver-portal.client/karma.conf.js
+++ driver-portal/src/driver-portal.client/karma.conf.js
@@ -1,4 +1,4 @@
-// Karma configuration file, see link for more information
+﻿// Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 //const path = require('path');
 
