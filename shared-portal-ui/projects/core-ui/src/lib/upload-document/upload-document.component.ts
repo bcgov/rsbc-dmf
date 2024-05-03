7dcfadfcc82c798012cd0eaf09f06d7ec6e6503d
shruthi-rapala <shruthi.rapala@quartech.com>
2024-05-03 11:05:47 -0700

    Removed drop zone from upload document in shared portal

--- shared-portal-ui/projects/core-ui/src/lib/upload-document/upload-document.component.ts
+++ shared-portal-ui/projects/core-ui/src/lib/upload-document/upload-document.component.ts
@@ -6,14 +6,12 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import {
-  MatLabel,
-  MatError,
   MatFormFieldModule,
 } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { NgxDropzoneModule } from 'ngx-dropzone';
+//import { MatIconModule } from '@angular/material/icon';
+//import { NgxDropzoneModule } from 'ngx-dropzone';
 import { MatOptionModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -32,8 +30,8 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     MatCheckboxModule,
     MatSelectModule,
     MatOptionModule,
-    NgxDropzoneModule,
-    MatIconModule,
+    //NgxDropzoneModule,
+    //MatIconModule,
   ],
   templateUrl: './upload-document.component.html',
   styleUrl: './upload-document.component.scss',
