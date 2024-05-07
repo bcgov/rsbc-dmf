855c5674e38b707b714f2923fe5b6e767d2055b5
shruthi-rapala <shruthi.rapala@quartech.com>
2024-05-07 12:56:14 -0700

    DFTDP#151 Medical Practitioner Portal Dashboard - Integarated backend api for seraching the case by case Id

---
+++ medical-practitioner-portal/src/app/shared/api/models/patient-case.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export interface PatientCase {
+  birthDate?: string | null;
+  caseId?: string | null;
+  dmerType?: string | null;
+  driverLicenseNumber?: string | null;
+  name?: string | null;
+  status?: string | null;
+}
--- medical-practitioner-portal/src/app/shared/api/services/cases.service.ts
+++ medical-practitioner-portal/src/app/shared/api/services/cases.service.ts
@@ -9,16 +9,68 @@ import { BaseService } from '../base-service';
 import { ApiConfiguration } from '../api-configuration';
 import { StrictHttpResponse } from '../strict-http-response';
 
+import { apiCasesCaseIdGet$Json } from '../fn/cases/api-cases-case-id-get-json';
+import { ApiCasesCaseIdGet$Json$Params } from '../fn/cases/api-cases-case-id-get-json';
+import { apiCasesCaseIdGet$Plain } from '../fn/cases/api-cases-case-id-get-plain';
+import { ApiCasesCaseIdGet$Plain$Params } from '../fn/cases/api-cases-case-id-get-plain';
 import { apiCasesGet$Json } from '../fn/cases/api-cases-get-json';
 import { ApiCasesGet$Json$Params } from '../fn/cases/api-cases-get-json';
 import { apiCasesGet$Plain } from '../fn/cases/api-cases-get-plain';
 import { ApiCasesGet$Plain$Params } from '../fn/cases/api-cases-get-plain';
 import { DmerCaseListItem } from '../models/dmer-case-list-item';
+import { PatientCase } from '../models/patient-case';
 
 @Injectable({ providedIn: 'root' })
 export class CasesService extends BaseService {
   constructor(config: ApiConfiguration, http: HttpClient) {
     super(config, http);
+  }
+
+  /** Path part for operation `apiCasesCaseIdGet()` */
+  static readonly ApiCasesCaseIdGetPath = '/api/Cases/{caseId}';
+
+  /**
+   * This method provides access to the full `HttpResponse`, allowing access to response headers.
+   * To access only the response body, use `apiCasesCaseIdGet$Plain()` instead.
+   *
+   * This method doesn't expect any request body.
+   */
+  apiCasesCaseIdGet$Plain$Response(params: ApiCasesCaseIdGet$Plain$Params, context?: HttpContext): Observable<StrictHttpResponse<PatientCase>> {
+    return apiCasesCaseIdGet$Plain(this.http, this.rootUrl, params, context);
+  }
+
+  /**
+   * This method provides access only to the response body.
+   * To access the full response (for headers, for example), `apiCasesCaseIdGet$Plain$Response()` instead.
+   *
+   * This method doesn't expect any request body.
+   */
+  apiCasesCaseIdGet$Plain(params: ApiCasesCaseIdGet$Plain$Params, context?: HttpContext): Observable<PatientCase> {
+    return this.apiCasesCaseIdGet$Plain$Response(params, context).pipe(
+      map((r: StrictHttpResponse<PatientCase>): PatientCase => r.body)
+    );
+  }
+
+  /**
+   * This method provides access to the full `HttpResponse`, allowing access to response headers.
+   * To access only the response body, use `apiCasesCaseIdGet$Json()` instead.
+   *
+   * This method doesn't expect any request body.
+   */
+  apiCasesCaseIdGet$Json$Response(params: ApiCasesCaseIdGet$Json$Params, context?: HttpContext): Observable<StrictHttpResponse<PatientCase>> {
+    return apiCasesCaseIdGet$Json(this.http, this.rootUrl, params, context);
+  }
+
+  /**
+   * This method provides access only to the response body.
+   * To access the full response (for headers, for example), `apiCasesCaseIdGet$Json$Response()` instead.
+   *
+   * This method doesn't expect any request body.
+   */
+  apiCasesCaseIdGet$Json(params: ApiCasesCaseIdGet$Json$Params, context?: HttpContext): Observable<PatientCase> {
+    return this.apiCasesCaseIdGet$Json$Response(params, context).pipe(
+      map((r: StrictHttpResponse<PatientCase>): PatientCase => r.body)
+    );
   }
 
   /** Path part for operation `apiCasesGet()` */
