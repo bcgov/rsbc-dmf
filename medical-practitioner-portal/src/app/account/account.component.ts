74858543b4b07583191f31a474b468b366c942e1
shruthi-rapala <shruthi.rapala@quartech.com>
2024-05-14 12:09:52 -0700

    DFTDP # 151 Medical Practitioner Portal Dashboard - Updated styles on the filter section on dashboard Updated button on the medical portal for to be compitable accesibility

--- medical-practitioner-portal/src/app/account/account.component.ts
+++ medical-practitioner-portal/src/app/account/account.component.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-account',
   standalone: true,
-  imports: [MatCardModule, MatIconModule, CommonModule],
+  imports: [MatCardModule, MatIconModule, CommonModule, MatButtonModule],
   templateUrl: './account.component.html',
-  styleUrl: './account.component.scss'
+  styleUrl: './account.component.scss',
 })
 export class AccountComponent {
   isExpanded: Record<string, boolean> = {};
