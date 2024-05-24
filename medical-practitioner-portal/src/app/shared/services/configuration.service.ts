7366f7dd804bd7a0d2be31128cf72bc14e5e6a59
Jebb Burditt <jebb.burditt@gmail.com>
2024-05-24 12:32:35 -0700

    cleanup config service

--- medical-practitioner-portal/src/app/shared/services/configuration.service.ts
+++ medical-practitioner-portal/src/app/shared/services/configuration.service.ts
@@ -1,18 +1,18 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { Inject, Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { tap } from 'rxjs/operators';
-import { environment } from '@src/environments/environment.prod';
+//import { environment } from '@src/environments/environment.prod';
 import { Configuration } from '../api/models';
 import { ConfigService } from '../api/services';
-import { KeycloakOptions } from 'keycloak-angular';
+//import { KeycloakOptions } from 'keycloak-angular';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ConfigurationService {
+  public onLoaded: BehaviorSubject<boolean> = new BehaviorSubject(false);
   private config: Configuration | null = null;
-  private keycloakOptions: KeycloakOptions | null = null;
 
   constructor(
     @Inject(APP_BASE_HREF) public baseHref: string,
@@ -26,18 +26,16 @@ export class ConfigurationService {
     return this.configurationService.apiConfigGet$Json().pipe(
       tap((c) => {
         this.config = { ...c };
+        this.onLoaded.next(true);
       })
     );
   }
 
-  public isConfigured(): boolean {
-    return this.config !== null;
-  }
+  // public isConfigured(): boolean {
+  //   return this.config !== null;
+  // }
 
-  public getKeycloakOptions(): KeycloakOptions {
-    if (!this.keycloakOptions) {
-      this.keycloakOptions = environment.keycloakOptions as KeycloakOptions;
-    }
-    return this.keycloakOptions || {};
-  }
+  // public getKeycloakOptions(): KeycloakOptions {
+  //   return this.config?.keycloakOptions || {};
+  // }
 }
