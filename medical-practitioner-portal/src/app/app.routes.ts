e68d90f35e452023d49346468f7b92af4ce4a604
Jebb Burditt <jebb.burditt@gmail.com>
2024-05-29 09:27:29 -0700

    add auth guard to all pages add denied access page

--- medical-practitioner-portal/src/app/app.routes.ts
+++ medical-practitioner-portal/src/app/app.routes.ts
@@ -4,15 +4,21 @@ import { CaseDetailsComponent } from './case-details/case-details.component';
 import { AccountComponent } from './account/account.component';
 import { GetHelpComponent } from './get-help/get-help.component';
 import { CaseSubmissionsComponent } from '../case-submissions/case-submissions.component';
+import { AuthenticationGuard } from './features/auth/guards/authentication.guard';
+import { DeniedComponent } from './denied/denied.component';
 
+export const ROUTE_DENIED = 'denied';
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: ROUTE_DENIED, component: DeniedComponent },
   {
-    path: 'dashboard',
-    component: DashboardComponent,
+    path: '', canActivateChild:[AuthenticationGuard], children:
+    [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'caseDetails/:caseId', component: CaseDetailsComponent },
+      { path: 'account', component: AccountComponent },
+      { path: 'getHelp', component: GetHelpComponent },
+      { path: 'caseSubmissions', component: CaseSubmissionsComponent },
+    ]
   },
-  { path: 'caseDetails/:caseId', component: CaseDetailsComponent },
-  { path: 'account', component: AccountComponent },
-  { path: 'getHelp', component: GetHelpComponent },
-  { path: 'caseSubmissions', component: CaseSubmissionsComponent },
 ];
