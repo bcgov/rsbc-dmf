0ce7e173e13afdf7d4d7ab9ad63225963cca5135
Jebb Burditt <jebb.burditt@gmail.com>
2024-06-05 11:47:38 -0700

    remove some of the controller and keycloak code

--- medical-portal/src/OneHealthAdapter.Service/Startup.cs
+++ medical-portal/src/OneHealthAdapter.Service/Startup.cs
@@ -30,44 +30,39 @@ public class Startup
 
         services
           .AddHttpClients(config)
-          // keycloak auth needed for keycloak role management - I don't think we will need this or the keycloak role management, if we did, I think this should be moved to a new
-          // project that would serve the keycloak role management controllers. Role management will likely be managed by OneHealth UI
-          //.AddKeycloakAuth(config)
           .AddSingleton<IClock>(NodaTime.SystemClock.Instance)
           .AddSingleton<Microsoft.Extensions.Logging.ILogger>(svc => svc.GetRequiredService<ILogger<Startup>>());
 
         if (!string.IsNullOrEmpty(Configuration["Jwt:Secret"]))
-            {
+        {
             byte[] key = Encoding.UTF8.GetBytes(Configuration["Jwt:Secret"]);
-                Array.Resize(ref key, 32);
+            Array.Resize(ref key, 32);
 
-                // Configure JWT authentication
-                services.AddAuthentication(o =>
+            // Configure JWT authentication
+            services.AddAuthentication(o =>
+            {
+                o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(o =>
+            {
+                o.SaveToken = true;
+                o.RequireHttpsMetadata = false;
+                o.TokenValidationParameters = new TokenValidationParameters
                 {
-                    o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                    o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                }).AddJwtBearer(o =>
-                {
-                    o.SaveToken = true;
-                    o.RequireHttpsMetadata = false;
-                    o.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        RequireExpirationTime = false,
+                    RequireExpirationTime = false,
                     ValidIssuer = Configuration["Jwt:Issuer"],
                     ValidAudience = Configuration["Jwt:Audience"],
-                        IssuerSigningKey =
-                            new SymmetricSecurityKey(key)
-                    };
-                });
-            }
-            else
-            {
-                services.AddAuthentication();
-            }
-                
-        services.AddAuthorization();
+                    IssuerSigningKey =
+                        new SymmetricSecurityKey(key)
+                };
+            });
+        }
+        else
+        {
+            services.AddAuthentication();
+        }
 
-        services.AddControllers().AddNewtonsoftJson(x => x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+        services.AddAuthorization();
 
         services.AddHttpClient();
 
@@ -85,48 +80,6 @@ public class Startup
 
         services.AddHealthChecks()
             .AddCheck("liveliness", () => HealthCheckResult.Healthy());
-
-        services.AddApiVersioning(options =>
-        {
-            options.ReportApiVersions = true;
-            options.AssumeDefaultVersionWhenUnspecified = true;
-            options.ApiVersionReader = new HeaderApiVersionReader("api-version");
-        });
-
-        services.AddSwaggerGen(options =>
-        {
-            options.SwaggerDoc("v1", new OpenApiInfo { Title = "Pidp Adapter API", Version = "v1" });
-            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-            {
-                Description = "Standard Authorization header using the Bearer scheme. Example: \"bearer {token}\"",
-                In = ParameterLocation.Header,
-                Name = "Authorization",
-                Type = SecuritySchemeType.ApiKey
-            });
-            options.AddSecurityRequirement(new OpenApiSecurityRequirement()
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            },
-                            Scheme = "oauth2",
-                            Name = "Bearer",
-                            In = ParameterLocation.Header,
-
-                        },
-                        new List<string>()
-                    }
-                });
-            options.OperationFilter<SecurityRequirementsOperationFilter>();
-            options.CustomSchemaIds(x => x.FullName);
-        });
-
-        services.AddCors(setupAction => setupAction.AddPolicy(Constants.CorsPolicy, corsPolicyBuilder => corsPolicyBuilder.WithOrigins(config.Settings.Cors.AllowedOrigins)));
-        services.AddFluentValidationRulesToSwagger();
     }
 
     private Configuration InitializeConfiguration(IServiceCollection services)
@@ -148,8 +101,6 @@ public class Startup
             app.UseDeveloperExceptionPage();
          
         }
-        app.UseSwagger();
-        app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v1/swagger.json", "Pidp Api Adapter"));
 
         app.UseSerilogRequestLogging(options => options.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
         {
@@ -160,13 +111,11 @@ public class Startup
             }
         });
         app.UseRouting();
-        app.UseCors(Constants.CorsPolicy);
         app.UseAuthentication();
         app.UseAuthorization();
         app.UseEndpoints(endpoints =>
         {
             endpoints.MapGrpcService<OneHealthService>();
-            endpoints.MapControllers();
             endpoints.MapGet("/", async context =>
             {
                 await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client");
