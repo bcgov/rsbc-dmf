d945a0e2a937885feaf311582da7f892b8556676
Mihai Listov <mihailistov@gmail.com>
2024-06-13 11:08:08 -0700

    DFTDP-73: cleanup console logs

--- medical-practitioner-portal/src/app/shared/api/fn/chefs/api-chefs-submission-json-put.ts
+++ medical-practitioner-portal/src/app/shared/api/fn/chefs/api-chefs-submission-json-put.ts
@@ -18,13 +18,10 @@ export function apiChefsSubmissionPut(
   params?: ApiChefsSubmissionPut$Params,
   context?: HttpContext,
 ): Observable<StrictHttpResponse<ChefsSubmission>> {
-  console.log('*******************apiChefsSubmissionPut*******************');
   const rb = new RequestBuilder(rootUrl, apiChefsSubmissionPut.PATH, 'put');
   if (params) {
     rb.body(params.body, 'application/*+json');
   }
-  console.log(rb);
-  console.log('*******************apiChefsSubmissionPut*******************');
   return http
     .request(
       rb.build({ responseType: 'json', accept: 'application/json', context }),
@@ -32,13 +29,6 @@ export function apiChefsSubmissionPut(
     .pipe(
       filter((r: any): r is HttpResponse<any> => r instanceof HttpResponse),
       map((r: HttpResponse<any>) => {
-        console.log(
-          '*******************apiChefsSubmissionPut RESPONSE*******************',
-        );
-        console.log(r);
-        console.log(
-          '*******************apiChefsSubmissionPut RESPONSE*******************',
-        );
         return r as StrictHttpResponse<any>;
       }),
     );
