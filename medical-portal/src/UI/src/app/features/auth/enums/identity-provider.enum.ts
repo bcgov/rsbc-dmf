671f0936c77b766eea1ccd5b54ef1ea620684327
Jebb Burditt <jebb.burditt@gmail.com>
2024-06-21 14:15:09 -0700

    fix role description by adding a mapping

--- medical-portal/src/UI/src/app/features/auth/enums/identity-provider.enum.ts
+++ medical-portal/src/UI/src/app/features/auth/enums/identity-provider.enum.ts
@@ -8,3 +8,8 @@ export enum Role {
   Practitioner = 'PRACTITIONER',
   Enrolled = 'DMFT_ENROLLED'
 }
+
+export const RoleDescription = new Map<Role, string>([
+  [Role.Practitioner, 'Practitioner'],
+  [Role.Enrolled, 'Enrolled']
+])
