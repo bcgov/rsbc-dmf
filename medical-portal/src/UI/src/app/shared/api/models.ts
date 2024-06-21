064e571438d4064927e901224327e4a759b96e55
Jebb Burditt <jebb.burditt@gmail.com>
2024-06-21 12:37:10 -0700

    add back chefssubmission model

--- medical-portal/src/UI/src/app/shared/api/models.ts
+++ medical-portal/src/UI/src/app/shared/api/models.ts
@@ -2,7 +2,7 @@
 /* eslint-disable */
 export { ApplicationVersionInfo } from './models/application-version-info';
 export { CaseDocument } from './models/case-document';
-
+export { ChefsSubmission } from './models/chefs-submission';
 export { DmerCaseListItem } from './models/dmer-case-list-item';
 export { Document } from './models/document';
 export { DocumentSubTypes } from './models/document-sub-types';
