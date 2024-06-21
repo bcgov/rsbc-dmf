57cb8ad3a7442555b63d3a5f2265a1c7da426f5b
Jebb Burditt <jebb.burditt@gmail.com>
2024-06-21 09:46:39 -0700

    add todo

--- cms-adapter/src/Rsbc.Dmf.CaseManagement/Model/Dto/Case.cs
+++ cms-adapter/src/Rsbc.Dmf.CaseManagement/Model/Dto/Case.cs
@@ -10,6 +10,7 @@ namespace Rsbc.Dmf.CaseManagement.Dto
         // TODO Rename to IDCode
         public string CaseNumber { get; set; }
 
+        // TODO rename DueDate
         // dfp_latestcompliancedate
         public DateTimeOffset? LatestComplianceDate { get; set; }
 
