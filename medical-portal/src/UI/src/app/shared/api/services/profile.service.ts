e2da5d67cbc2cb3c28f4c4f74aece8d81078726f
Jebb Burditt <jebb.burditt@gmail.com>
2024-06-21 12:46:43 -0700

    remove unnecessary profile services

--- medical-portal/src/UI/src/app/shared/api/services/profile.service.ts
+++ medical-portal/src/UI/src/app/shared/api/services/profile.service.ts
@@ -13,14 +13,6 @@ import { apiProfileCurrentGet$Json } from '../fn/profile/api-profile-current-get
 import { ApiProfileCurrentGet$Json$Params } from '../fn/profile/api-profile-current-get-json';
 import { apiProfileCurrentGet$Plain } from '../fn/profile/api-profile-current-get-plain';
 import { ApiProfileCurrentGet$Plain$Params } from '../fn/profile/api-profile-current-get-plain';
-import { apiProfileEmailPut } from '../fn/profile/api-profile-email-put';
-import { ApiProfileEmailPut$Params } from '../fn/profile/api-profile-email-put';
-import { apiProfilePractitionerRolesDelete } from '../fn/profile/api-profile-practitioner-roles-delete';
-import { ApiProfilePractitionerRolesDelete$Params } from '../fn/profile/api-profile-practitioner-roles-delete';
-import { apiProfilePractitionerRolesGet } from '../fn/profile/api-profile-practitioner-roles-get';
-import { ApiProfilePractitionerRolesGet$Params } from '../fn/profile/api-profile-practitioner-roles-get';
-import { apiProfilePractitionerRolesPut } from '../fn/profile/api-profile-practitioner-roles-put';
-import { ApiProfilePractitionerRolesPut$Params } from '../fn/profile/api-profile-practitioner-roles-put';
 import { UserProfile } from '../models/user-profile';
 
 @Injectable({ providedIn: 'root' })
@@ -73,106 +65,6 @@ export class ProfileService extends BaseService {
   apiProfileCurrentGet$Json(params?: ApiProfileCurrentGet$Json$Params, context?: HttpContext): Observable<UserProfile> {
     return this.apiProfileCurrentGet$Json$Response(params, context).pipe(
       map((r: StrictHttpResponse<UserProfile>): UserProfile => r.body)
-    );
-  }
-
-  /** Path part for operation `apiProfileEmailPut()` */
-  static readonly ApiProfileEmailPutPath = '/api/Profile/email';
-
-  /**
-   * This method provides access to the full `HttpResponse`, allowing access to response headers.
-   * To access only the response body, use `apiProfileEmailPut()` instead.
-   *
-   * This method sends `application/*+json` and handles request body of type `application/*+json`.
-   */
-  apiProfileEmailPut$Response(params?: ApiProfileEmailPut$Params, context?: HttpContext): Observable<StrictHttpResponse<void>> {
-    return apiProfileEmailPut(this.http, this.rootUrl, params, context);
-  }
-
-  /**
-   * This method provides access only to the response body.
-   * To access the full response (for headers, for example), `apiProfileEmailPut$Response()` instead.
-   *
-   * This method sends `application/*+json` and handles request body of type `application/*+json`.
-   */
-  apiProfileEmailPut(params?: ApiProfileEmailPut$Params, context?: HttpContext): Observable<void> {
-    return this.apiProfileEmailPut$Response(params, context).pipe(
-      map((r: StrictHttpResponse<void>): void => r.body)
-    );
-  }
-
-  /** Path part for operation `apiProfilePractitionerRolesGet()` */
-  static readonly ApiProfilePractitionerRolesGetPath = '/api/Profile/practitionerRoles';
-
-  /**
-   * This method provides access to the full `HttpResponse`, allowing access to response headers.
-   * To access only the response body, use `apiProfilePractitionerRolesGet()` instead.
-   *
-   * This method doesn't expect any request body.
-   */
-  apiProfilePractitionerRolesGet$Response(params?: ApiProfilePractitionerRolesGet$Params, context?: HttpContext): Observable<StrictHttpResponse<void>> {
-    return apiProfilePractitionerRolesGet(this.http, this.rootUrl, params, context);
-  }
-
-  /**
-   * This method provides access only to the response body.
-   * To access the full response (for headers, for example), `apiProfilePractitionerRolesGet$Response()` instead.
-   *
-   * This method doesn't expect any request body.
-   */
-  apiProfilePractitionerRolesGet(params?: ApiProfilePractitionerRolesGet$Params, context?: HttpContext): Observable<void> {
-    return this.apiProfilePractitionerRolesGet$Response(params, context).pipe(
-      map((r: StrictHttpResponse<void>): void => r.body)
-    );
-  }
-
-  /** Path part for operation `apiProfilePractitionerRolesPut()` */
-  static readonly ApiProfilePractitionerRolesPutPath = '/api/Profile/practitionerRoles';
-
-  /**
-   * This method provides access to the full `HttpResponse`, allowing access to response headers.
-   * To access only the response body, use `apiProfilePractitionerRolesPut()` instead.
-   *
-   * This method sends `application/*+json` and handles request body of type `application/*+json`.
-   */
-  apiProfilePractitionerRolesPut$Response(params?: ApiProfilePractitionerRolesPut$Params, context?: HttpContext): Observable<StrictHttpResponse<void>> {
-    return apiProfilePractitionerRolesPut(this.http, this.rootUrl, params, context);
-  }
-
-  /**
-   * This method provides access only to the response body.
-   * To access the full response (for headers, for example), `apiProfilePractitionerRolesPut$Response()` instead.
-   *
-   * This method sends `application/*+json` and handles request body of type `application/*+json`.
-   */
-  apiProfilePractitionerRolesPut(params?: ApiProfilePractitionerRolesPut$Params, context?: HttpContext): Observable<void> {
-    return this.apiProfilePractitionerRolesPut$Response(params, context).pipe(
-      map((r: StrictHttpResponse<void>): void => r.body)
-    );
-  }
-
-  /** Path part for operation `apiProfilePractitionerRolesDelete()` */
-  static readonly ApiProfilePractitionerRolesDeletePath = '/api/Profile/practitionerRoles';
-
-  /**
-   * This method provides access to the full `HttpResponse`, allowing access to response headers.
-   * To access only the response body, use `apiProfilePractitionerRolesDelete()` instead.
-   *
-   * This method sends `application/*+json` and handles request body of type `application/*+json`.
-   */
-  apiProfilePractitionerRolesDelete$Response(params?: ApiProfilePractitionerRolesDelete$Params, context?: HttpContext): Observable<StrictHttpResponse<void>> {
-    return apiProfilePractitionerRolesDelete(this.http, this.rootUrl, params, context);
-  }
-
-  /**
-   * This method provides access only to the response body.
-   * To access the full response (for headers, for example), `apiProfilePractitionerRolesDelete$Response()` instead.
-   *
-   * This method sends `application/*+json` and handles request body of type `application/*+json`.
-   */
-  apiProfilePractitionerRolesDelete(params?: ApiProfilePractitionerRolesDelete$Params, context?: HttpContext): Observable<void> {
-    return this.apiProfilePractitionerRolesDelete$Response(params, context).pipe(
-      map((r: StrictHttpResponse<void>): void => r.body)
     );
   }
 
