90e8455212244e25e5864bc1986f01b48ad0dc4f
Mihai Listov <mihailistov@gmail.com>
2024-06-25 11:57:35 -0700

    DFTDP-73: Cleanup unused GetDriverCaseAndDocuments endpoint and models

--- medical-portal/src/UI/src/app/shared/api/models.ts
+++ medical-portal/src/UI/src/app/shared/api/models.ts
@@ -1,21 +1,15 @@
 /* tslint:disable */
 /* eslint-disable */
 export { ApplicationVersionInfo } from './models/application-version-info';
-export { Case } from './models/case';
 export { CaseDocument } from './models/case-document';
 export { ChefsBundle } from './models/chefs-bundle';
 export { ChefsSubmission } from './models/chefs-submission';
 export { DmerCaseListItem } from './models/dmer-case-list-item';
-export { Document } from './models/document';
-export { DocumentSubType } from './models/document-sub-type';
 export { DocumentSubTypes } from './models/document-sub-types';
-export { DocumentType } from './models/document-type';
 export { DriverInfoReply } from './models/driver-info-reply';
 export { Endorsement } from './models/endorsement';
 export { Licence } from './models/licence';
 export { PatientCase } from './models/patient-case';
-export { Person } from './models/person';
 export { ProblemDetails } from './models/problem-details';
 export { ResultStatus } from './models/result-status';
-export { Timestamp } from './models/timestamp';
 export { UserProfile } from './models/user-profile';
--- medical-portal/src/UI/src/app/shared/api/models/document-sub-type.ts
+++
@@ -1,6 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export interface DocumentSubType {
-  id?: number;
-  name?: string | null;
-}
