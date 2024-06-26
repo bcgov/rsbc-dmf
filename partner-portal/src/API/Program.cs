b177b51f5c62506658df0ca7dd222e3cf9ded5e2
Jebb Burditt <jebb.burditt@gmail.com>
2024-06-26 15:20:19 -0700

    working keycloak auth based on medical portal keycloak without policies

--- partner-portal/src/API/Program.cs
+++ partner-portal/src/API/Program.cs
@@ -1,12 +1,13 @@
-﻿//using Microsoft.AspNetCore.Authentication;
+﻿using Keycloak.AuthServices.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-//using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 using Serilog;
+using System.Data;
 using System.Net;
-//using System.Security.Claims;
+using System.Security.Claims;
 
 Console.WriteLine($"Starting Partner Portal Api.");
 var builder = WebApplication.CreateBuilder(args);
@@ -19,6 +20,52 @@ var env = builder.Environment;
 var isDevelopment = env.EnvironmentName == "Development";
 
 var config = builder.Configuration;
+
+
+services.AddKeycloakWebApiAuthentication(
+    keycloakOptions =>
+    {
+        keycloakOptions.Realm = config["KEYCLOAK_REALM"];
+        keycloakOptions.Audience = config["KEYCLOAK_AUDIENCE"];
+        keycloakOptions.AuthServerUrl = config["KEYCLOAK_URL"];
+    },
+    jwtBearerOptions =>
+    {
+        jwtBearerOptions.Events = new JwtBearerEvents
+        {
+            OnTokenValidated = async context => await OnTokenValidatedAsync(context),
+            OnAuthenticationFailed = context =>
+            {
+                Log.Error(context.Exception, "Error validating bearer token");
+                context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                return Task.CompletedTask;
+            }
+        };
+    });
+
+services.AddAuthorization(options =>
+{
+    //options.AddPolicy(
+    //    Policies.Oidc,
+    //    policy => policy
+    //        // confirm this is working by using a bad secret, currently the secret is not being validated
+    //        .RequireAuthenticatedUser().AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+    //// TODO verify if we need to add scope medical-portal-ui and medical-portal-api or if just medical-portal will do, in other projects there are api and ui
+    //// the below does not work, since the scope claim looks something like "email profile openid". This problem has already been solved, research the proper way to handle scope
+    //// need to add the scope to keycloak admin UI before we can add the scope to FE, which would pass the scope claim to the BE
+    ////.RequireClaim(Claims.Scope, "medical-portal")
+    //);
+
+    //options.AddPolicy(
+    //    Policies.MedicalPractitioner,
+    //    policy => policy
+    //    .RequireAuthenticatedUser()
+    //    .RequireRole(Claims.IdentityProvider, Roles.Practitoner, Roles.Moa));
+    //options.AddPolicy(Policies.Enrolled, policy => policy
+    //.RequireAuthenticatedUser()
+    //    .RequireRole(Claims.IdentityProvider, Roles.Dmft));
+});
+
 services.AddSerilogBootstrapLogger();
 services.AddCmsAdapterGrpcService(config);
 Console.WriteLine($"cms-adapter grpc service registered.");
@@ -32,8 +79,6 @@ services.AddCors(options =>
             .AllowAnyMethod()
             .AllowAnyHeader());
 });
-
-// TODO add authentication here
 
 services.AddControllers();
 
@@ -56,9 +101,14 @@ try
     app.UseCors(corsPolicy);
     app.UseSwagger();
     app.UseSwaggerUI();
-    //app.UseAuthentication();
-    //app.UseAuthorization();
-    app.MapControllers();
+    app.UseAuthentication();
+    app.UseAuthorization();
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllers();
+        //    .RequireAuthorization(Policies.MedicalPractitioner, Policies.Enrolled, Policies.Oidc);
+    });
+    //app.MapControllers();
     app.MapSwagger();
     app.Run();
     Console.WriteLine("Program configuration completed.");
@@ -70,4 +120,26 @@ catch (Exception ex)
 finally
 {
     Log.CloseAndFlush();
+}
+
+async Task OnTokenValidatedAsync(TokenValidatedContext context)
+{
+    if (context.Principal?.Identity is ClaimsIdentity identity
+        && identity.IsAuthenticated)
+    {
+        // Flatten the Resource Access claim
+        //identity.AddClaims(
+        //    identity.GetResourceAccessRoles(Clients.License)
+        //        .Select(role => new Claim(identity.RoleClaimType, role))
+        //);
+
+        //identity.AddClaims(
+        //    identity.GetResourceAccessRoles(Clients.DmftStatus)
+        //        .Select(role => new Claim(identity.RoleClaimType, role))
+        //);
+
+        // TODO I think this is wrong, we should only need to call this once but this is validating on every request
+        //var userService = context.HttpContext.RequestServices.GetRequiredService<IUserService>();
+        //context.Principal = await userService.Login(context.Principal);
+    }
 }
---
+++ partner-portal/src/UI/src/app/shared/services/configuration.service.ts
@@ -0,0 +1,60 @@
+import { APP_BASE_HREF } from '@angular/common';
+import { Inject, Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
+//import { environment } from '@src/environments/environment.prod';
+// import { Configuration } from '../api/models';
+//import { ConfigService } from '../api/services';
+//import { KeycloakOptions } from 'keycloak-angular';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ConfigurationService {
+  public onLoaded: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  private config: unknown | null = null;
+
+  constructor(
+    @Inject(APP_BASE_HREF) public baseHref: string,
+    //private configurationService: ConfigService
+  ) {}
+
+  public load() {
+    if (this.config != null) {
+      return of(this.config);
+    }
+    // return this.configurationService.apiConfigGet().pipe(
+    //   tap((c: any) => {
+    //     this.config = { ...c };
+    //     this.onLoaded.next(true);
+    //   })
+    // );
+    this.config =   {
+      "Keycloak": {
+        "RealmUrl": "https://common-logon-test.hlth.gov.bc.ca/auth/realms/moh_applications",
+        "Config": {
+          "Url": "https://common-logon-test.hlth.gov.bc.ca/auth",
+          "Audience": "LICENCE-STATUS",
+          "Realm": "moh_applications",
+          "ClientId": "DMFT-WEBAPP"
+        },
+        "InitOptions": {
+          "OnLoad": "check-sso",
+          "Flow": "standard",
+          "ResponseMode": "fragment",
+          "PkceMethod": "S256"
+        }
+      },
+    };
+    this.onLoaded.next(true);
+    return true;
+  }
+
+  // public isConfigured(): boolean {
+  //   return this.config !== null;
+  // }
+
+  // public getKeycloakOptions(): KeycloakOptions {
+  //   return this.config?.keycloakOptions || {};
+  // }
+}
