16707789cc4e3811f050c22b256f2b5e755b02dd
Jebb Burditt <jebb.burditt@gmail.com>
2024-06-28 17:04:19 -0700

    working simple auth feature flag

--- medical-portal/src/UI/src/app/features/auth/services/auth.service.ts
+++ medical-portal/src/UI/src/app/features/auth/services/auth.service.ts
@@ -3,6 +3,7 @@ import { Observable, from, of } from 'rxjs';
 import { KeycloakService } from 'keycloak-angular';
 import { KeycloakLoginOptions } from 'keycloak-js';
 import { Role } from '../enums/identity-provider.enum';
+import { ProfileManagementService } from '@app/shared/services/profile.service';
 
 export interface IAuthService {
   login(options?: KeycloakLoginOptions): Observable<void>;
@@ -14,7 +15,10 @@ export interface IAuthService {
   providedIn: 'root',
 })
 export class AuthService implements IAuthService {
-  public constructor(private keycloakService: KeycloakService) {}
+  public constructor(
+    private keycloakService: KeycloakService,
+    private profileManagementService: ProfileManagementService
+  ) {}
 
   public login(options?: KeycloakLoginOptions): Observable<void> {
     return from(this.keycloakService.login(options));
@@ -33,8 +37,9 @@ export class AuthService implements IAuthService {
   }
 
   public hasAccess(): boolean {
-    console.info('getUserRoles', this.keycloakService.getUserRoles());
-    return this.keycloakService.isLoggedIn() && this.keycloakService.isUserInRole(Role.Enrolled);
+    //console.info('getUserRoles', this.keycloakService.getUserRoles());
+    //return this.keycloakService.isLoggedIn() && this.keycloakService.isUserInRole(Role.Enrolled);
+    return !!this.profileManagementService.getCachedProfile().roles?.includes(Role.Enrolled);
   }
 
   public getRoles(): Role[] {
@@ -47,6 +52,7 @@ export class AuthService implements IAuthService {
   }
 
   public logout(redirectUri: string): Observable<void> {
+    sessionStorage.removeItem("profile");
     return from(this.keycloakService.logout(redirectUri));
   }
 }
