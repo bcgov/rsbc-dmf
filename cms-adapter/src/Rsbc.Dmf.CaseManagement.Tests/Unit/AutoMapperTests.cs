a67d77ac284f6d0f9fbf1a6b5220883b3b17c735
shruthi-rapala <shruthi.rapala@quartech.com>
2024-07-02 11:29:00 -0700

    DFTDP # 156 Medical Practitioner Portal - eDMER Status Tracking - Added FaxReceived Date to document View Model. - Fix the submission date on driver documents - Added the new feild to document mapping

--- cms-adapter/src/Rsbc.Dmf.CaseManagement.Tests/Unit/AutoMapperTests.cs
+++ cms-adapter/src/Rsbc.Dmf.CaseManagement.Tests/Unit/AutoMapperTests.cs
@@ -223,6 +223,7 @@ namespace Rsbc.Dmf.CaseManagement.Tests.Unit
             document.DocumentUrl = "DocumentUrl";
             document.SubmittalStatus = "Received";
             document.IdCode = "HG123";
+            document.FaxReceivedDate = DateTimeOffset.Now;
 
             var mappedDocument = _mapper.Map<Service.Document>(document);
 
@@ -238,6 +239,7 @@ namespace Rsbc.Dmf.CaseManagement.Tests.Unit
             Assert.Equal(document.DocumentUrl, mappedDocument.DocumentUrl);
             Assert.Equal(document.SubmittalStatus, mappedDocument.SubmittalStatus);
             Assert.Equal(document.IdCode, mappedDocument.IdCode);
+            Assert.Equal(document.FaxReceivedDate, mappedDocument.FaxReceivedDate.ToDateTimeOffset());
         }
     }
 }
