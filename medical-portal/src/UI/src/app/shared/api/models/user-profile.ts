3306fe0c398023c859d8ad27feab84475560fe05
Jebb Burditt <jebb.burditt@gmail.com>
2024-07-10 13:36:20 -0700

    replace endorsement service with profile endorsements

--- medical-portal/src/UI/src/app/shared/api/models/user-profile.ts
+++ medical-portal/src/UI/src/app/shared/api/models/user-profile.ts
@@ -1,7 +1,9 @@
 /* tslint:disable */
 /* eslint-disable */
+import { Endorsement } from '../models/endorsement';
 export interface UserProfile {
   email?: string | null;
+  endorsements?: Array<Endorsement> | null;
   firstName?: string | null;
   id?: string | null;
   lastName?: string | null;
