93d79bbcfaded9bd7f7566ace529b67480e7dcf1
Jebb Burditt <jebb.burditt@gmail.com>
2024-07-16 09:01:57 -0700

    revert formatting change, for consistency

--- partner-portal/src/API/ViewModels/Driver.cs
+++ partner-portal/src/API/ViewModels/Driver.cs
@@ -1,26 +1,27 @@
-﻿namespace Rsbc.Dmf.PartnerPortal.Api.ViewModels;
-
-public class Driver
+﻿namespace Rsbc.Dmf.PartnerPortal.Api.ViewModels
 {
-    public bool? Flag51 { get; set; }
+    public class Driver
+    {
+        public bool? Flag51 { get; set; }
 
-    /// <summary>
-    /// Last Name
-    /// </summary>
-    public string LastName { get; set; }
+        /// <summary>
+        /// Last Name
+        /// </summary>
+        public string LastName { get; set; }
 
-    /// <summary>
-    /// License Number
-    /// </summary>
-    public string LicenseNumber { get; set; }
+        /// <summary>
+        /// License Number
+        /// </summary>
+        public string LicenseNumber { get; set; }
 
-    /// <summary>
-    /// True if loaded from ICBC
-    /// </summary>
-    public bool? LoadedFromICBC { get; set; }
+        /// <summary>
+        /// True if loaded from ICBC
+        /// </summary>
+        public bool? LoadedFromICBC { get; set; }
 
-    /// <summary>
-    /// The date this particular case had the medical issue date.
-    /// </summary>
-    public DateTimeOffset? MedicalIssueDate { get; set; }
+        /// <summary>
+        /// The date this particular case had the medical issue date.
+        /// </summary>
+        public DateTimeOffset? MedicalIssueDate { get; set; }
+    }
 }
