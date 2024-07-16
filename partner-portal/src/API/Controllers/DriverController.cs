6524552f9110c6ede06a486f4453e3761da3a019
Jebb Burditt <jebb.burditt@gmail.com>
2024-07-15 19:47:16 -0700

    partial work in progress changes

---
+++ partner-portal/src/API/Controllers/DriverController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Pssg.Dmf.IcbcAdapter.Client;
+using Rsbc.Dmf.IcbcAdapter;
+using Rsbc.Dmf.PartnerPortal.ViewModels;
+using System.Net;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DriverController : Controller
+{
+    private readonly ICachedIcbcAdapterClient _icbcAdapterClient;
+    private readonly ILogger _logger;
+    private readonly IConfiguration _configuration;
+
+    public DriverController(ICachedIcbcAdapterClient icbcAdapterClient, ILogger logger, IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration = configuration;
+    }
+
+    [HttpGet("info/{driverLicenceNumber}")]
+    [ProducesResponseType(typeof(IEnumerable<Document>), (int)HttpStatusCode.OK)]
+    [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+    [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+    [ActionName(nameof(GetHistory))]
+    public async Task<ActionResult<DriverInfoReply>> GetHistory([FromRoute]string driverLicenceNumber)
+    {
+        try
+        {
+            var request = new DriverInfoRequest();
+            request.DriverLicence = driverLicenceNumber;
+            var reply = await _icbcAdapterClient.GetDriverInfoAsync(request);
+
+            return Json(reply);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"{nameof(GetHistory)} failed.");
+            return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+        }
+    }
+}
---
+++ partner-portal/src/API/Rsbc.Dmf.PartnerPortal.Api.Tests/Integration/DriverTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Rsbc.Dmf.PartnerPortal.ViewModels;
+using Xunit;
+
+public class DriverTests : ApiIntegrationTestBase
+{
+    public DriverTests(IConfiguration configuration) : base(configuration) { }
+
+    [Fact]
+    public async Task Get_Driver_History()
+    {
+        var driverLicenceNumber = _configuration["TEST_ICBC_DL"];
+        if (string.IsNullOrEmpty(driverLicenceNumber))
+            return;
+
+        var request = new HttpRequestMessage(HttpMethod.Get, $"{DRIVER_API_BASE}/info/{driverLicenceNumber}");
+        var clientResult = await HttpClientSendRequest<Document>(request);
+
+        Assert.Equal(driverLicenceNumber, clientResult.Driver.LicenseNumber);
+    }
+}
