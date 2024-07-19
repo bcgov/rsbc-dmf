907e6238f3b2bbd37bf32430674a8ac1562f433e
Mihai Listov <mihailistov@gmail.com>
2024-07-18 17:36:51 -0700

    DFTDP-155: Add assign and priority to chefs submission payload

--- medical-portal/src/API/RSBC.DMF.MedicalPortal.API/ViewModels/ChefsSubmission.cs
+++ medical-portal/src/API/RSBC.DMF.MedicalPortal.API/ViewModels/ChefsSubmission.cs
@@ -12,8 +12,8 @@ public static class SubmissionStatus
 public class ChefsSubmission
 {
     [JsonProperty("status")] public string Status { get; set; } = SubmissionStatus.Draft;
-
     [JsonProperty("submission")] public Dictionary<string, object> Submission { get; set; } = new();
-
     [JsonProperty("flags")] public Dictionary<string, object> Flags { get; set; } = new();
+    [JsonProperty("priority")] public string Priority { get; set; } = new("");
+    [JsonProperty("assign")] public string Assign { get; set; } = new("");
 }
