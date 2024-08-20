506079ae94d5d4b3781b22e851b532cbecb1a549
Jebb Burditt <jebb.burditt@gmail.com>
2024-08-19 21:13:48 -0700

    add angular path to workspace and fix angular package versions to match

--- shared-portal-library/projects/shared-portal-lib/src/public-api.ts
+++ shared-portal-library/projects/shared-portal-lib/src/public-api.ts
@@ -4,3 +4,23 @@
 
 export * from './lib/shared-portal-lib.service';
 export * from './lib/shared-portal-lib.component';
+
+// Layout
+export *from './lib/layout/header/header.component'
+//export *from './lib/layout/footer/footer.component'
+//export *from './lib/layout/nav-menu/nav-menu.component'
+
+// Case Definitions
+// export *from './lib/case-definitions/case-status/case-status.component'
+// export *from './lib/case-definitions/case-type/case-type.component'
+// export *from './lib/case-definitions/decision-outcome/decision-outcome.component'
+// export *from './lib/case-definitions/dmer-type/dmer-type.component'
+// export *from './lib/case-definitions/eligible-license-class/eligible-license-class.component'
+// export *from './lib/case-definitions/letter-topic/letter-topic.component'
+// export *from './lib/case-definitions/rsbc-case-assignment/rsbc-case-assignment.component'
+// export *from './lib/case-definitions/submission-status/submission-status.component'
+// export *from './lib/case-definitions/submission-type/submission-type.component'
+// export *from './lib/case-definitions/dmer-status/dmer-status.component'
+
+// // Upload-Document
+// export *from './lib/upload-document/upload-document.component'
