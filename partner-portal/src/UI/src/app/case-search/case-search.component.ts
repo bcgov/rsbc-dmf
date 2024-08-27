a1cb9ae32437a37d6dcfe7be6ce606b5d654aacf
shruthi-rapala <shruthi.rapala@quartech.com>
2024-08-27 11:22:21 -0700

    DFDP # 260 Partner Portal - Case Search - Fixed the mat tabs on the case search tab - Added comments button to open the modal popup

--- partner-portal/src/UI/src/app/case-search/case-search.component.ts
+++ partner-portal/src/UI/src/app/case-search/case-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
 import { MatToolbar } from '@angular/material/toolbar';
@@ -14,6 +14,11 @@ import { SubmissionHistoryComponent } from '@app/submission-history/submission-h
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { CaseManagementService } from '@app/shared/services/case-management/case-management.service';
 import { CaseSearch } from '@app/shared/api/models';
+import { MatTabsModule } from '@angular/material/tabs';
+import { LettersToDriverComponent } from '@app/letters-to-driver/letters-to-driver.component';
+import { GetAssistanceComponent } from '@app/get-assistance/get-assistance.component';
+import { CommentsComponent } from '@app/comments/comments.component';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-case-search',
@@ -32,6 +37,11 @@ import { CaseSearch } from '@app/shared/api/models';
     SubmissionRequirementsComponent,
     SubmissionHistoryComponent,
     RouterLink,
+    MatTabsModule,
+    LettersToDriverComponent,
+    GetAssistanceComponent,
+    CommentsComponent,
+    MatDialogModule
   ],
   templateUrl: './case-search.component.html',
   styleUrl: './case-search.component.scss',
@@ -41,6 +51,8 @@ export class CaseSearchComponent implements OnInit{
   idCode = '';
 
   @ViewChild(MatAccordion) accordion!: MatAccordion;
+  
+  readonly dialog = inject(MatDialog);
   
   caseDetails: CaseSearch = this.router.getCurrentNavigation()?.extras.state as CaseSearch;
   
@@ -53,4 +65,19 @@ export class CaseSearchComponent implements OnInit{
   ngOnInit(): void {
   }
   
+  openCommentsDialog() {
+    const dialogRef = this.dialog.open(CommentsComponent, {
+      height: '730px',
+      width: '400px',
+      position: {
+        bottom: '8px',
+        right: '8px',
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
 }
