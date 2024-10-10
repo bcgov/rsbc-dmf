3919d63c2d52b52f93b79fb31fcdf106b428d67a
shruthi-rapala <shruthi.rapala@quartech.com>
2024-10-10 13:44:15 -0700

    DFTDP# 281 Move recent-case to shared library - Added the most recent componet to shared libarary - Added app.model enum file to shared folder - Added the recent case and enums to public API - Added the new shared most recent component to driver serach and case search

---
+++ shared-portal-ui/projects/core-ui/src/lib/recent-case/recent-case.component.ts
@@ -0,0 +1,122 @@
+// IMPORTANT keep this file identical to partner-portal recent-case.component
+
+import { CUSTOM_ELEMENTS_SCHEMA, Component, Input, OnInit, ViewChild, input } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+// import { CaseManagementService } from '../shared/services/case-management/case-management.service';
+import { Router, RouterLink } from '@angular/router';
+// import { CaseDetail } from '../shared/api/models';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+import { MatStepper, MatStep, MatStepLabel, MatStepperIcon } from '@angular/material/stepper';
+import { DatePipe } from '@angular/common';
+import { MatAccordion, MatExpansionPanel, MatExpansionPanelHeader, MatExpansionPanelTitle } from '@angular/material/expansion';
+import { MatIcon } from '@angular/material/icon';
+import { MatCard, MatCardContent } from '@angular/material/card';
+import { CaseStageEnum } from '../app.model';
+import{PortalsEnum} from '../app.model';
+
+@Component({
+    selector: 'app-shared-recent-case',
+    templateUrl: './recent-case.component.html',
+    styleUrls: ['./recent-case.component.scss'],
+    providers: [
+      {
+          provide: STEPPER_GLOBAL_OPTIONS,
+          useValue: { displayDefaultIndicatorType: false },
+      },
+    ],
+    standalone: true,
+    imports: [
+        MatCard,
+        RouterLink,
+        MatCardContent,
+        MatStepper,
+        MatStep,
+        MatStepLabel,
+        MatStepperIcon,
+        MatIcon,
+        MatExpansionPanel,
+        MatExpansionPanelHeader,
+        MatExpansionPanelTitle,
+        DatePipe,
+        MatAccordion
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class RecentCaseComponent implements OnInit {
+  public caseDetails?: any; // CaseDetail | undefined;
+
+  @Input() caseManagementService: any;
+  @Input() portal!: PortalsEnum;
+
+  PortalsEnum = PortalsEnum;
+  
+
+  selectedIndex = 0;
+  panelOpenState = false;
+  // Partner Portal
+  idCode = '';
+
+  @ViewChild('stepper') stepper!: MatStepper;
+
+  constructor(
+    // private caseManagementService: CaseManagementService,
+    private breakpointObserver: BreakpointObserver,
+    private router: Router
+  ) {}
+
+  // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
+  ngAfterViewInit(): void {
+    this.breakpointObserver
+      .observe(['(max-width: 768px)'])
+      .subscribe((result) => {
+        if (result.matches) {
+          // this.stepper._stepsList.toArray()[this.selectedIndex].expanded = true;
+          this.stepper.orientation = 'vertical';
+        } else {
+          this.stepper.orientation = 'horizontal';
+        }
+      });
+  }
+
+  public ngOnInit(): void {
+    this.caseManagementService
+      .getMostRecentCase()
+      .subscribe((recentCase: any) => {
+        this.caseDetails = recentCase;
+        if (recentCase.status === CaseStageEnum.Opened) {
+          this.selectedIndex = 0;
+        }
+        if (recentCase.status === CaseStageEnum.OpenPendingSubmission) {
+          this.selectedIndex = 1;
+        }
+        if (recentCase.status === CaseStageEnum.UnderReview) {
+          this.selectedIndex = 2;
+        }
+        if (recentCase.status === CaseStageEnum.FileEndTasks) {
+          this.selectedIndex = 3;
+        }
+        if (recentCase.status === CaseStageEnum.IntakeValidation) {
+          this.selectedIndex = 4;
+        }
+        if (recentCase.status === CaseStageEnum.Closed) {
+          this.selectedIndex = 5;
+        }
+      });
+  }
+
+
+  // partner Portal
+  searchByCaseId(){
+    this.caseManagementService.searchByCaseId({idCode: this.idCode})
+    .subscribe({
+      next: (caseDetails: any) => {
+        this.router.navigate(['/caseSearch', this.idCode as string], {state: caseDetails});
+      },
+      error: (error: any) => {
+      
+        console.error('error', error);
+      }
+  
+    });
+  }
+}
--- shared-portal-ui/projects/core-ui/src/public-api.ts
+++ shared-portal-ui/projects/core-ui/src/public-api.ts
@@ -5,12 +5,12 @@
 export * from './lib/core-ui.service';
 export * from './lib/core-ui.component';
 
-// Layout
+//Layout
 export *from './lib/layout/header/header.component'
 export *from './lib/layout/footer/footer.component'
 export *from './lib/layout/nav-menu/nav-menu.component'
 
-// Case Definitions
+//Case Definitions
 export *from './lib/case-definitions/case-status/case-status.component'
 export *from './lib/case-definitions/case-type/case-type.component'
 export *from './lib/case-definitions/decision-outcome/decision-outcome.component'
@@ -22,5 +22,13 @@ export *from './lib/case-definitions/submission-status/submission-status.compone
 export *from './lib/case-definitions/submission-type/submission-type.component'
 export *from './lib/case-definitions/dmer-status/dmer-status.component'
 
-// Upload-Document
+//Upload-Document
 export *from './lib/upload-document/upload-document.component'
+
+//Recent Case
+
+export *from './lib/recent-case/recent-case.component'
+
+
+//Enums
+export *from './lib/app.model'
