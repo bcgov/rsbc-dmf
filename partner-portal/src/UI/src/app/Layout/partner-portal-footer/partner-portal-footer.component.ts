54ae5c5588f88131dfb3e0b88f4c1aa3c80ab615
shruthi-rapala <shruthi.rapala@quartech.com>
2024-10-15 14:04:53 -0700

    DFTDP # 197 Partner Portal - User Interface - Fixed the navigation issue with get help button - Fixed styling issues on the cancel callback popup

--- partner-portal/src/UI/src/app/Layout/partner-portal-footer/partner-portal-footer.component.ts
+++ partner-portal/src/UI/src/app/Layout/partner-portal-footer/partner-portal-footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-partner-portal-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './partner-portal-footer.component.html',
   styleUrl: './partner-portal-footer.component.scss',
 })
