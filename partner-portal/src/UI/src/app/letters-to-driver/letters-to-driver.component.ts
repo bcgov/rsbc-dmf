4e73dc6f3c39d7df738ee7f420021bef3ebe2092
shruthi-rapala <shruthi.rapala@quartech.com>
2024-10-16 14:28:52 -0700

    DFTDP# 279 Move letters-to-driver to shared library - Partner Portal - Updated the letters to driver component to use the shared libarary component

--- partner-portal/src/UI/src/app/letters-to-driver/letters-to-driver.component.ts
+++ partner-portal/src/UI/src/app/letters-to-driver/letters-to-driver.component.ts
@@ -1,17 +1,7 @@
-// IMPORTANT keep this file identical to driver-portal letters-to-driver.component
-
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
-import { MatAccordion, MatExpansionPanel, MatExpansionPanelHeader, MatExpansionPanelTitle } from '@angular/material/expansion';
-import { CaseManagementService } from '../shared/services/case-management/case-management.service';
-import { Document } from '../shared/api/models';
-import { MatButton } from '@angular/material/button';
-import { MatIcon } from '@angular/material/icon';
-import { MatCard, MatCardContent } from '@angular/material/card';
-import { NgFor, NgClass, NgIf, DatePipe } from '@angular/common';
+import { Component } from '@angular/core';
 import { QuickLinksComponent } from '../quick-links/quick-links.component';
-import { CaseTypeComponent } from '../../../../../../shared-portal-ui/projects/core-ui/src/lib/case-definitions/case-type/case-type.component';
-import { LetterTopicComponent } from '../../../../../../shared-portal-ui/projects/core-ui/src/lib/case-definitions/letter-topic/letter-topic.component';
-import { SubmittalStatusEnum } from '@app/app.model';
+import { CaseManagementService } from '../shared/services/case-management/case-management.service';
+import { SharedLettersToDriverComponent} from '@shared/core-ui';
 
 @Component({
   selector: 'app-letters-to-driver',
@@ -19,108 +9,13 @@ import { SubmittalStatusEnum } from '@app/app.model';
   styleUrls: ['./letters-to-driver.component.scss'],
   standalone: true,
   imports: [
-    QuickLinksComponent,
-    NgFor,
-    MatCard,
-    NgClass,
-    MatCardContent,
-    MatIcon,
-    MatButton,
-    NgIf,
-    MatAccordion,
-    MatExpansionPanel,
-    MatExpansionPanelHeader,
-    MatExpansionPanelTitle,
-    CaseTypeComponent,
-    LetterTopicComponent,
-    DatePipe,
+      QuickLinksComponent,
+      SharedLettersToDriverComponent
+    
   ],
 })
-export class LettersToDriverComponent implements OnInit {
-  constructor(private caseManagementService: CaseManagementService) {}
-
-  @ViewChild(MatAccordion) accordion!: MatAccordion;
-  isExpanded: Record<string, boolean> = {};
-  pageSize = 10;
-  @Input() isLoading = true;
-  filteredDocuments?: Document[] | null = [];
-
-  private _letterDocuments?: Document[] | null = [];
-
-  get letterDocuments() {
-    return this._letterDocuments;
-  }
-
-  @Input()
-  set letterDocuments(documents: Document[] | null | undefined) {
-    this._letterDocuments = documents;
-    this._letterDocuments?.forEach((doc) => {
-      if (doc.documentId) this.isExpanded[doc.documentId] = false;
-    });
-
-    this.filteredDocuments = this._letterDocuments?.slice(0, this.pageSize);
-  }
-
-  letterOutDocuments: Document[] = [];
-
-  ngOnInit(): void {
-    window.scrollTo(0, 0);
-    this.getLetterOutDocument();
-  }
-
-  toggleIsExpandable(id?: string | null) {
-    if (id) this.isExpanded[id] = !this.isExpanded[id];
-  }
-
-  viewMore() {
-    const pageSize = (this.filteredDocuments?.length ?? 0) + this.pageSize;
-
-    this.filteredDocuments = this._letterDocuments?.slice(0, pageSize);
-  }
-
-  downloadLetters(documentId: string | null | undefined) {
-    if (!documentId) return;
-    this.caseManagementService
-      .getDownloadDocument({ documentId })
-      .subscribe((res) => {
-        this.downloadFile(res);
-      });
-  }
-
-  downloadFile(data: any) {
-    const blob = new Blob([data], { type: 'application/pdf' });
-    const url = window.URL.createObjectURL(blob);
-    //window.open('https://path/to/file.extenstion', '_blank');
-    //window.open(url, '_blank');
-    const link = document.createElement('a');
-    link.href = url;
-    link.download = 'LetterOut.pdf';
-    link.click();
-  }
-
-  getLetterOutDocument() {
-    this.caseManagementService.getAllDriverDocuments()
-      .subscribe((letterDocuments: any) => {
-        if (!letterDocuments) {
-          return;
-        }
-        this._letterDocuments = letterDocuments;
-        this.letterOutDocuments = [];
-        letterDocuments.forEach((letter: any) => {
-          if (
-            [SubmittalStatusEnum.Issued, SubmittalStatusEnum.Sent].includes(
-              letter.submittalStatus as SubmittalStatusEnum,
-            )
-          ) {
-            this.letterOutDocuments.push(letter);
-          }
-        });
-
-        this.filteredDocuments = this.letterOutDocuments.slice(
-          0,
-          this.pageSize,
-        );
-        this.isLoading = false;
-      });
-  }
+export class LettersToDriverComponent 
+{
+  constructor(public caseManagementService: CaseManagementService) 
+    {}
 }
