e39abb23c95cf6a249e376ada8474f2eb18b5b68
shruthi-rapala <shruthi.rapala@quartech.com>
2024-10-23 14:42:35 -0700

    DEleted enums from the get assistace ts file

--- partner-portal/src/UI/src/app/get-assistance/get-assistance.component.ts
+++ partner-portal/src/UI/src/app/get-assistance/get-assistance.component.ts
@@ -4,21 +4,6 @@ import { UserService } from '../shared/services/user.service';
 import { SharedGetAssistanceComponent } from '@shared/core-ui';
 
 
-interface CallBackTopic {
-  value: string;
-  viewValue: string;
-}
-
-enum HelpTopics {
-  ALL_TOPICS = 0,
-  UPLOAD_EXTRA_DOCUMENT = 1,
-  VIEW_DMER_SUBMISSION = 2,
-  RECEIVED_AN_INCORRECT_LETTER = 3,
-  DOCUMENT_HISTORY = 4,
-  REPLACEMENT_DOCUMENT = 5,
-  REQUEST_EXTENSION = 6,
-}
-
 @Component({
   selector: 'app-get-assistance',
   templateUrl: './get-assistance.component.html',
