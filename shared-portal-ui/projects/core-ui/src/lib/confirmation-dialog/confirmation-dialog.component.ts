d37de7368937a749da9ad396b2b042ebf95cbbbe
Jebb Burditt <jebb.burditt@gmail.com>
2024-11-01 13:49:39 -0700

    cleanup confirmation dialog styling

--- shared-portal-ui/projects/core-ui/src/lib/confirmation-dialog/confirmation-dialog.component.ts
+++ shared-portal-ui/projects/core-ui/src/lib/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,7 +1,6 @@
 import { Component, ElementRef, Inject, ViewChild, TemplateRef } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog, MatDialogActions, MatDialogRef } from '@angular/material/dialog';
 import { MatIcon } from '@angular/material/icon';
-//import { ConfigurationService } from '@app/shared/services/configuration.service';
 
 @Component({
   selector: 'app-confirm-dialog',
@@ -11,26 +10,16 @@ import { MatIcon } from '@angular/material/icon';
   styleUrl: './confirmation-dialog.component.scss',
 })
 export class ConfirmationDialogComponent {
-  //caseId: string;
-  //documentId: string;
   public title: string = "Confirm";
   public confirmMessage: string = 'Are you sure?';
+  public confirmButtonText: string = 'Ok';
+  public cancelButtonText: string = 'Cancel';
 
   constructor(
-    //private popupService: PopupService,
-    //private configService: ConfigurationService,
     //@Inject(MAT_DIALOG_DATA) public data: { caseId: string, documentId: string },
     public dialogRef: MatDialogRef<ConfirmationDialogComponent>
   ) {
       //this.caseId = data.caseId;
       //this.documentId = data.documentId;
-  }
-
-  ngOnInit() {
-
-  }
-
-  closePopup() {
-    //this.popupService.closePopup();
   }
 }
