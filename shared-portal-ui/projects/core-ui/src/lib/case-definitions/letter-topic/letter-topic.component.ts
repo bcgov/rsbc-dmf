963a95098ab0e19d05058ac7c66d4302c5055b4f
shruthi-rapala <shruthi.rapala@quartech.com>
2024-11-08 10:30:41 -0800

    DFTDP # 292 Cleanup frontend and devop items - Fixed the gen api command in driver portal package.json - Fixed the css files to scss files for definition components

--- shared-portal-ui/projects/core-ui/src/lib/case-definitions/letter-topic/letter-topic.component.ts
+++ shared-portal-ui/projects/core-ui/src/lib/case-definitions/letter-topic/letter-topic.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-letter-topic',
     templateUrl: './letter-topic.component.html',
-    styleUrls: ['./letter-topic.component.css'],
+    styleUrls: ['./letter-topic.component.scss'],
     standalone: true
 })
 export class LetterTopicComponent {
--- shared-portal-ui/projects/core-ui/src/lib/case-definitions/rsbc-case-assignment/rsbc-case-assignment.component.ts
+++ shared-portal-ui/projects/core-ui/src/lib/case-definitions/rsbc-case-assignment/rsbc-case-assignment.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-rsbc-case-assignment',
     templateUrl: './rsbc-case-assignment.component.html',
-    styleUrls: ['./rsbc-case-assignment.component.css'],
+    styleUrls: ['./rsbc-case-assignment.component.scss'],
     standalone: true
 })
 export class RsbcCaseAssignmentComponent {
