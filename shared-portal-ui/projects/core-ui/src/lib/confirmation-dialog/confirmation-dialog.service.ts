c7c5f5cf79a565483ea1bde5ef41c7312d04a467
Jebb Burditt <jebb.burditt@gmail.com>
2024-11-13 10:43:43 -0800

    cleanup confirmation dialog service

--- shared-portal-ui/projects/core-ui/src/lib/confirmation-dialog/confirmation-dialog.service.ts
+++ shared-portal-ui/projects/core-ui/src/lib/confirmation-dialog/confirmation-dialog.service.ts
@@ -3,6 +3,15 @@ import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { map, Observable, tap } from 'rxjs';
 import { ConfirmationDialogComponent } from './confirmation-dialog.component';
 
+/*
+  To call this service from one of the portals, use code similar to the following:
+
+  openConfirmationDialog() {
+    this.confirmationDialogService.openDialog('350px', '220px', 'Are you sure you want to submit the form?', 'Submit', 'Cancel').subscribe((isConfirmed: boolean) => {
+      console.log("isConfirmed", isConfirmed);
+    });
+  }
+*/
 @Injectable({
   providedIn: 'root',
 })
