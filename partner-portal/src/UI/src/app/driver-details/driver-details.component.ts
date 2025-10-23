876868aa309e7a8daa1b19bb24061874d973c9a6
shruthi-rapala <shruthi.rapala@quartech.com>
2025-10-23 13:41:32 -0700

    DFCMSMOD # 205 Driver details Tab Added Driver details componnet Added the driver details tab to the menu Added route configuration in the app.route.ts file

---
+++ partner-portal/src/UI/src/app/driver-details/driver-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-driver-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './driver-details.component.html',
+  styleUrl: './driver-details.component.css'
+})
+export class DriverDetailsComponent {
+
+}
