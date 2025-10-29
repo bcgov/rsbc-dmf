bd312dbfff3a9202141915f541158ffd092226b0
shruthi-rapala <shruthi.rapala@quartech.com>
2025-10-29 10:08:24 -0700

    DFCMSMOD # 207 Rehab Interlock Detail Tab Added rehab interlock and remedial case details component Added the new tabs on the driver search component

---
+++ partner-portal/src/UI/src/app/rehab-interlock/rehab-interlock.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rehab-interlock',
+  standalone: true,
+  imports: [],
+  templateUrl: './rehab-interlock.component.html',
+  styleUrl: './rehab-interlock.component.scss'
+})
+export class RehabInterlockComponent {
+
+}
---
+++ partner-portal/src/UI/src/app/remedial-case-details/remedial-case-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-remedial-case-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './remedial-case-details.component.html',
+  styleUrl: './remedial-case-details.component.scss'
+})
+export class RemedialCaseDetailsComponent {
+
+}
