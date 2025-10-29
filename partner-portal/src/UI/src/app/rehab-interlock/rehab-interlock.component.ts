dcdf859dfa664a470f37fd1de17c96a81e52cf81
shruthi-rapala <shruthi.rapala@quartech.com>
2025-10-29 12:35:43 -0700

    Added Rehab Assignemnt and Ignitition interlock HTML sections Added required styles to the page

--- partner-portal/src/UI/src/app/rehab-interlock/rehab-interlock.component.ts
+++ partner-portal/src/UI/src/app/rehab-interlock/rehab-interlock.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-rehab-interlock',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatIconModule, MatExpansionModule,],
   templateUrl: './rehab-interlock.component.html',
   styleUrl: './rehab-interlock.component.scss'
 })
 export class RehabInterlockComponent {
 
+  toggleIsExpandable(): void {
+    // this.isExpandable = !this.isExpandable;
+  }
 }
