a365b9cbcb398b85cf080a6b8a1b797bc481e7b6
shruthi-rapala <shruthi.rapala@quartech.com>
2025-11-06 13:16:01 -0800

    Added definitions section to Remedial case details component - Added Case Type, Case Status, DMER Type, Decision Outcome, Eligible License Class definitions - Updated the component file to include these definistions from shared component

--- partner-portal/src/UI/src/app/remedial-case-details/remedial-case-details.component.ts
+++ partner-portal/src/UI/src/app/remedial-case-details/remedial-case-details.component.ts
@@ -1,21 +1,31 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ViewChild } from '@angular/core';
+import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
 import { CaseManagementService } from '@app/shared/services/case-management/case-management.service';
 import { RecentCaseComponent, CaseStatusComponent, CaseTypeComponent, DecisionOutcomeComponent, EligibleLicenseClassComponent, PortalsEnum } from '@shared/core-ui';
+import { MedicalDmerTypesComponent } from '../../app/definitions/medical-dmer-types/medical-dmer-types.component';
 @Component({
   selector: 'app-remedial-case-details',
   standalone: true,
-  imports: [RecentCaseComponent],
+  imports: [
+    RecentCaseComponent,
+    MatExpansionModule,
+    CaseStatusComponent,
+    CaseTypeComponent,
+    DecisionOutcomeComponent,
+    EligibleLicenseClassComponent,
+    MedicalDmerTypesComponent],
   templateUrl: './remedial-case-details.component.html',
   styleUrl: './remedial-case-details.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class RemedialCaseDetailsComponent {
 
-    PortalsEnum = PortalsEnum;
+  PortalsEnum = PortalsEnum;
+  @ViewChild(MatAccordion) accordion!: MatAccordion;
 
-     constructor(
-         public caseManagementService: CaseManagementService,
-         //private userService: UserService,
-         //private route: ActivatedRoute
-         ) {}
+  constructor(
+    public caseManagementService: CaseManagementService,
+    //private userService: UserService,
+    //private route: ActivatedRoute
+  ) { }
 }
