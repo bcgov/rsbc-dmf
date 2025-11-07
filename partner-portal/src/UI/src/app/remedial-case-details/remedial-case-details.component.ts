179a89e826ea896a2c70b2122b65c53acdad159d
shruthi-rapala <shruthi.rapala@quartech.com>
2025-11-07 14:55:06 -0800

    DFCMSMOD # 249 Remedial case details Tab - Closed Case Details Generated FE API after updating the backend Added Closed case section on the remedial case tab Added backend api call to query the api and also filter based on the program APi Updated styles for the closed case details

--- partner-portal/src/UI/src/app/remedial-case-details/remedial-case-details.component.ts
+++ partner-portal/src/UI/src/app/remedial-case-details/remedial-case-details.component.ts
@@ -1,8 +1,13 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, ViewChild } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, Input, OnInit, ViewChild } from '@angular/core';
 import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
 import { CaseManagementService } from '@app/shared/services/case-management/case-management.service';
 import { RecentCaseComponent, CaseStatusComponent, CaseTypeComponent, DecisionOutcomeComponent, EligibleLicenseClassComponent, PortalsEnum } from '@shared/core-ui';
 import { MedicalDmerTypesComponent } from '../../app/definitions/medical-dmer-types/medical-dmer-types.component';
+import { DatePipe, NgFor, NgIf } from '@angular/common';
+import { UserService } from '@app/shared/services/user.service';
+import { CaseDetail } from '@app/shared/api/models';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 @Component({
   selector: 'app-remedial-case-details',
   standalone: true,
@@ -13,17 +18,73 @@ import { MedicalDmerTypesComponent } from '../../app/definitions/medical-dmer-ty
     CaseTypeComponent,
     DecisionOutcomeComponent,
     EligibleLicenseClassComponent,
-    MedicalDmerTypesComponent],
+    MedicalDmerTypesComponent,
+    DatePipe,
+    NgIf,
+    NgFor,
+    MatIconModule,
+    MatCardModule
+  ],
   templateUrl: './remedial-case-details.component.html',
   styleUrl: './remedial-case-details.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class RemedialCaseDetailsComponent {
+export class RemedialCaseDetailsComponent implements OnInit {
 
+  isExpanded: Record<string, boolean> = {};
   PortalsEnum = PortalsEnum;
+  isLoading = true;
   @ViewChild(MatAccordion) accordion!: MatAccordion;
+
+
+
+  // Get Driver details
+  driverDetails = this.userService.getCachedriver();
 
   constructor(
     public caseManagementService: CaseManagementService,
+    public userService: UserService
   ) { }
+
+  ngOnInit(): void {
+    if (this.driverDetails.id) {
+      this.getClosedRemedialCases(this.driverDetails.id as string);
+    } else {
+      console.log('No user profile');
+    }
+  }
+  _closedCaseDetails: CaseDetail[] | null = [];
+
+  @Input() set closedCaseDetails(caseDetails: CaseDetail[] | null) {
+    if (caseDetails !== undefined) {
+      this._closedCaseDetails = caseDetails;
+
+      this._closedCaseDetails?.forEach((cases) => {
+        if (cases.caseId) this.isExpanded[cases.caseId] = false;
+      });
+    }
+  }
+
+  get closedCaseDetails() {
+    return this._closedCaseDetails;
+  }
+
+  getClosedRemedialCases(driverId: string, programArea: string = 'Remedial') {
+
+    this.caseManagementService.getClosedCases({ programArea: programArea }).subscribe({
+      next: (closedCases) => {
+        this.closedCaseDetails = closedCases;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.error('Error fetching remedial Closed Cases:', error);
+      },
+    });
+  }
+
+
+  toggleIsExpandable(id?: string | null) {
+    if (id) this.isExpanded[id] = !this.isExpanded[id];
+  }
+
 }
