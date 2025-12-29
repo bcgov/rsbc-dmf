f6675814793b9d98be9312bdf4630854b2455679
shruthi-rapala <shruthi.rapala@quartech.com>
2025-12-29 09:15:03 -0800

    DFCMSMOD # 308 Add frontend User Access Request Screen Removed the provideMomentDateAdapter pacakge from main.cs as this is not required

--- partner-portal/src/UI/src/main.ts
+++ partner-portal/src/UI/src/main.ts
@@ -11,7 +11,6 @@ import { environment } from './environments/environment';
 import { BearerTokenInterceptor } from '@shared/core-ui';
 import { provideKeycloak } from './app/modules/keycloak/keycloak.provider';
 import { AuthGuard } from '@app/modules/keycloak/keycloak.guard';
-import { provideMomentDateAdapter } from '@angular/material-moment-adapter';
 
 
 bootstrapApplication(AppComponent, {
@@ -37,7 +36,6 @@ bootstrapApplication(AppComponent, {
       deps: [PlatformLocation],
     },
      AuthGuard,
-    provideMomentDateAdapter()
   ],
 }).catch((err) => console.error(err));
 
