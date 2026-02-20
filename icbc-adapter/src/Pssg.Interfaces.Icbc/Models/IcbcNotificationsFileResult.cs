eef41a02be1551f4b4d153f57bb8ed2b22fca4eb
Fintan Roche <Fintan.Roche@quartech.com>
2026-02-19 16:31:46 -0800

    Auto case creation only remove processed files

---
+++ icbc-adapter/src/Pssg.Interfaces.Icbc/Models/IcbcNotificationsFileResult.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pssg.Interfaces.Models
+{
+    public class IcbcNotificationsFileResult
+    {
+        public List<IFormFile> NotificationFiles { get; set; }
+        public IEnumerable<string> ServerRelativeUrl { get; set; }
+    }
+}
